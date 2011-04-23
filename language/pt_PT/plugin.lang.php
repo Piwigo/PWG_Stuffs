a62f7e00711a96678ce7a3a382a9f5b91767d587
PsyKick <PsyKick@piwigo.org>
2011-04-23 15:43:25 +0000

    [extensions][PWG_Stuffs] Missing translation keys added pt_PT
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@10601 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pt_PT/plugin.lang.php
+++ language/pt_PT/plugin.lang.php
@@ -1,55 +1,55 @@
 <?php
 
-/*TODO*/$lang['stuffs_mods_management'] = 'Blocks management';
-/*TODO*/$lang['stuffs_add_mod'] = 'Add a block';
-/*TODO*/$lang['stuffs_edit_mod'] = 'Edit block';
+$lang['stuffs_mods_management'] = 'Gestão de Blocos';
+$lang['stuffs_add_mod'] = 'Adicionar um bloco';
+$lang['stuffs_edit_mod'] = 'Editar bloco';
 
-/*TODO*/$lang['stuffs_module_title'] = 'Block title';
-/*TODO*/$lang['stuffs_desc'] = 'Description';
-/*TODO*/$lang['stuffs_only_for_admin'] = '(only for administration pannel)';
-/*TODO*/$lang['stuffs_type'] = 'Block type';
-/*TODO*/$lang['stuffs_position'] = 'Position';
-/*TODO*/$lang['stuffs_id_ligne'] = 'ID line';
-/*TODO*/$lang['stuffs_width'] = 'Width';
-/*TODO*/$lang['stuffs_action'] = 'Actions';
-/*TODO*/$lang['stuffs_edit'] = 'Modify block';
-/*TODO*/$lang['stuffs_delete'] = 'Delete block';
-/*TODO*/$lang['stuffs_confirm_delete'] = 'Are you sure you want to delete block %s?';
-/*TODO*/$lang['stuffs_main_block'] = 'Main block';
-/*TODO*/$lang['Hide on home page'] = 'Hide on home page';
+$lang['stuffs_module_title'] = 'Título do bloco';
+$lang['stuffs_desc'] = 'Descrição';
+$lang['stuffs_only_for_admin'] = '(apenas para o painel administrativo)';
+$lang['stuffs_type'] = 'Tipo de Bloco';
+$lang['stuffs_position'] = 'Posição';
+$lang['stuffs_id_ligne'] = 'ID da linha';
+$lang['stuffs_width'] = 'Largura';
+$lang['stuffs_action'] = 'Acções';
+$lang['stuffs_edit'] = 'Modificar bloco';
+$lang['stuffs_delete'] = 'Apagar bloco';
+$lang['stuffs_confirm_delete'] = 'Tem a certeza que deseja apagar o bloco %s?';
+$lang['stuffs_main_block'] = 'Bloco principal';
+$lang['Hide on home page'] = 'Esconder na página de entrada';
 
-/*TODO*/$lang['stuffs_add_module'] = 'Add new block';
-/*TODO*/$lang['stuffs_module_config'] = 'Block configuration';
-/*TODO*/$lang['stuffs_visual_config'] = 'Display configuration';
-/*TODO*/$lang['stuffs_module_options'] = 'Block options';
-/*TODO*/$lang['stuffs_authorized_group'] = 'Authorized groups';
-/*TODO*/$lang['stuffs_authorized_users'] = 'Authorized users';
-/*TODO*/$lang['stuffs_public_module'] = '(Keep empty for public block)';
-/*TODO*/$lang['stuffs_show_title'] = 'Show block title';
-/*TODO*/$lang['stuffs_show_module_on'] = 'Show block :';
-/*TODO*/$lang['stuffs_show_on_home'] = 'On main page';
-/*TODO*/$lang['stuffs_show_on_root'] = 'On categories root page';
-/*TODO*/$lang['stuffs_show_on_cats'] = 'On all categories pages';
-/*TODO*/$lang['stuffs_show_on_picture'] = 'On picture pages';
-/*TODO*/$lang['stuffs_no_options'] = 'No options for this block';
+$lang['stuffs_add_module'] = 'Adicionar novo bloco';
+$lang['stuffs_module_config'] = 'Configuração do bloco';
+$lang['stuffs_visual_config'] = 'Mostrar configuração';
+$lang['stuffs_module_options'] = 'Opções do bloco';
+$lang['stuffs_authorized_group'] = 'Grupos autorizados';
+$lang['stuffs_authorized_users'] = 'Utilizadores autorizados';
+$lang['stuffs_public_module'] = '(Manter vazio para bloco público)';
+$lang['stuffs_show_title'] = 'Mostrar título do bloco';
+$lang['stuffs_show_module_on'] = 'Mostrar bloco :';
+$lang['stuffs_show_on_home'] = 'Na página de entrada';
+$lang['stuffs_show_on_root'] = 'Na página raiz das categorias';
+$lang['stuffs_show_on_cats'] = 'Em todas as páginas de categorias';
+$lang['stuffs_show_on_picture'] = 'Nas páginas de Imagem';
+$lang['stuffs_no_options'] = 'Este bloco não tem opções';
 
-/*TODO*/$lang['stuffs_perm'] = 'Activate authorization management:';
-/*TODO*/$lang['stuffs_group_perm'] = 'by groups';
-/*TODO*/$lang['stuffs_user_perm'] = 'by user type';
-/*TODO*/$lang['stuffs_level_perm'] = 'by privacy level';
+$lang['stuffs_perm'] = 'Activar autorização  de gestão:';
+$lang['stuffs_group_perm'] = 'por grupos';
+$lang['stuffs_user_perm'] = 'por tipo de utilizador';
+$lang['stuffs_level_perm'] = 'por nível de privacidade';
 
-/*TODO*/$lang['stuffs_select_all'] = 'Select all';
-/*TODO*/$lang['stuffs_unselect_all'] = 'Unselect all';
+$lang['stuffs_select_all'] = 'Seleccionar todos';
+$lang['stuffs_unselect_all'] = 'Desseleccionar todos';
 
 // Infos messages
-/*TODO*/$lang['stuffs_order_saved'] = 'Order successfully saved.';
-/*TODO*/$lang['stuffs_upgrade_message'] = 'PWG Stuffs plugin as been renamed. It is now called "Blocks".';
+$lang['stuffs_order_saved'] = 'Ordem salva com sucesso.';
+$lang['stuffs_upgrade_message'] = 'A extensão PWG Stuffs foi renomeada. Agora chama-se "Blocos".';
 
 // Errors messages
-/*TODO*/$lang['stuffs_error_no_mod_selected'] = 'You didn\'t select any block.';
-/*TODO*/$lang['stuffs_no_name'] = 'You must specify block\'s name';
-/*TODO*/$lang['stuffs_parent_plugin_is_missing'] = 'PARENT PLUGIN IS NOT ACTIVATED. You should remove this block or re-activate parent plugin.';
-/*TODO*/$lang['stuffs_piclens_need_upgrade'] = 'PICLENS PLUGIN MUST BE UPDATED. PWG Stuffs is not compatible with this version of Piclens.';
+$lang['stuffs_error_no_mod_selected'] = 'Não seleccionou bloco algum.';
+$lang['stuffs_no_name'] = 'Deverá especificar um nome para o bloco';
+$lang['stuffs_parent_plugin_is_missing'] = 'A EXTENSÃO RAIZ NÃO ESTÁ ACTIVADA. Deverá remover este bloco ou re-activar a extensão raiz.';
+$lang['stuffs_piclens_need_upgrade'] = 'A EXTENSÃO PICLENS DEVERÁ SER ACTUALIZADA. PWG Stuffs não é compatível com esta versão da extensão Piclens.';
 
 //------------------------------------------------------------------------------
 //                            Module LastComs
