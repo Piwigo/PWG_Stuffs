c6000987aff3dae51e4b9a7128ac50862fe88995
ddtddt <ddtddt@piwigo.org>
2011-05-30 20:58:51 +0000

    [extensions] - PWG_Stuffs - add CN thanks to Em Ku
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@11114 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,155 @@
+<?php
+
+$lang['stuffs_mods_management'] = 'Quản lý các khối';
+$lang['stuffs_add_mod'] = 'Thêm một khối';
+$lang['stuffs_edit_mod'] = 'Chỉnh sửa khối';
+
+$lang['stuffs_module_title'] = 'Tiêu đề của khối';
+$lang['stuffs_desc'] = 'Mô tả';
+$lang['stuffs_only_for_admin'] = '(chỉ dành riêng cho bản điều khiển của quản trị viên)';
+$lang['stuffs_type'] = 'Kiểu khối';
+$lang['stuffs_position'] = 'Vị trí';
+$lang['stuffs_id_ligne'] = 'ID của hàng';
+$lang['stuffs_width'] = 'Chiều rộng';
+$lang['stuffs_action'] = 'Triển khai';
+$lang['stuffs_edit'] = 'Hiệu chỉnh khối';
+$lang['stuffs_delete'] = 'Xóa khối';
+$lang['stuffs_confirm_delete'] = 'Bạn có chắc là muốn xóa khối %s này?';
+$lang['stuffs_main_block'] = 'Khối chính';
+$lang['Hide on home page'] = 'Ẩn ở trang chủ';
+
+$lang['stuffs_add_module'] = 'Thêm khối mới';
+$lang['stuffs_module_config'] = 'Cấu hình khối';
+$lang['stuffs_visual_config'] = 'Hiển thị khối';
+$lang['stuffs_module_options'] = 'Tùy chọn khối';
+$lang['stuffs_authorized_group'] = 'Nhóm được ủy quyền';
+$lang['stuffs_authorized_users'] = 'Người dùng được ủy quyền';
+$lang['stuffs_public_module'] = '(Để trống những khối chung)';
+$lang['stuffs_show_title'] = 'Hiển thị tiêu đề khối';
+$lang['stuffs_show_module_on'] = 'Hiển thị khối :';
+$lang['stuffs_show_on_home'] = 'Trên trang chủ';
+$lang['stuffs_show_on_root'] = 'Trên trang của đề mục gốc';
+$lang['stuffs_show_on_cats'] = 'Trên tất cả những trang của đề mục';
+$lang['stuffs_show_on_picture'] = 'Trên những trang hình';
+$lang['stuffs_no_options'] = 'Không có tùy chọn nào cho khối này';
+
+$lang['stuffs_perm'] = 'Kích hoạt quản lý ủy quyền:';
+$lang['stuffs_group_perm'] = 'theo nhóm';
+$lang['stuffs_user_perm'] = 'theo kiểu người dùng';
+$lang['stuffs_level_perm'] = 'theo mức độ riêng tư';
+
+$lang['stuffs_select_all'] = 'Chọn tất cả';
+$lang['stuffs_unselect_all'] = 'Bỏ chọn tất cả';
+
+// Infos messages
+$lang['stuffs_order_saved'] = 'Vị trí đã được lưu thành công.';
+$lang['stuffs_upgrade_message'] = 'Plugin PWG Stuffs đã được đổi tên. Giờ nó là "Khối".';
+
+// Errors messages
+$lang['stuffs_error_no_mod_selected'] = 'Bạn đã không chọn khối nào.';
+$lang['stuffs_no_name'] = 'Bạn phải định rõ tên của khối';
+$lang['stuffs_parent_plugin_is_missing'] = 'PLUGIN GỐC KHÔNG ĐƯỢC KÍCH HOẠT. Ban phải gỏ bỏ khối này hoặc kích hoạt lại plugin gốc.';
+$lang['stuffs_piclens_need_upgrade'] = 'PLUGIN PICLENS PHẢI ĐƯỢC NÂNG CẤP. Plugin PWG Stuffs không tương thích với phiên bản hiện tại của plugin Piclens.';
+
+//------------------------------------------------------------------------------
+//              Module Lời bình cuối
+//------------------------------------------------------------------------------
+
+$lang['module_name_lastcoms'] = 'Lời bình cuối';
+$lang['module_desc_lastcoms'] = 'Hiển thị lời bình đã gởi sau cùng';
+
+$lang['lastcoms_no_nb_comments'] = 'Bạn phải định rõ số lời bình.';
+$lang['lastcoms_config'] = 'Cấu hình';
+$lang['lastcoms_nb_comments'] = 'Số lời bình cần hiển thị';
+$lang['lastcoms_show_buttons'] = 'Hiện nút của Admin';
+$lang['lastcoms_dim'] = 'Cỡ hình thu nhỏ';
+$lang['lastcoms_width'] = 'Chiều rộng lớn nhất';
+$lang['lastcoms_height'] = 'Chiều cao lớn nhất';
+$lang['lastcoms_nb_cadres'] = 'Số khung trên hàng';
+$lang['lastcoms_taille_cadres'] = 'Chiều cao của khung';
+
+//------------------------------------------------------------------------------
+//              Module Đăng nhập
+//------------------------------------------------------------------------------
+
+$lang['module_name_logon'] = 'Đăng nhập';
+$lang['module_desc_logon'] = 'Hiển thị khối đăng nhập';
+
+$lang['logon_remove_menubar_block'] = 'Bỏ khối "Đăng nhập nhanh" ra khỏi thanh trình đơn';
+
+//------------------------------------------------------------------------------
+//              Module Cá nhân
+//------------------------------------------------------------------------------
+
+$lang['module_name_personal'] = 'Khối cá nhân';
+$lang['module_desc_personal'] = 'Hiển thị khối cá nhân(like an edito)';
+
+$lang['personal_content'] = 'nội dung';
+
+//------------------------------------------------------------------------------
+//              Module Thẻ
+//------------------------------------------------------------------------------
+
+$lang['module_name_tags'] = 'Thẻ';
+$lang['module_desc_tags'] = 'Hiển thị thẻ thư viện';
+
+$lang['stuffs_tags_default_display'] = 'Thẻ hiển thị';
+$lang['stuffs_tags_cloud'] = 'Thẻ mây';
+$lang['stuffs_tags_letters'] = 'Nhóm theo ký tự';
+$lang['stuffs_tags_cumulus'] = 'Sử dụng chế độ mây tích (plugin <a href=http://piwigo.org/ext/extension_view.php?eid=263 target="_blank">Thẻ mây tích</a> phải được cài đặt)';
+
+//------------------------------------------------------------------------------
+//              Module Xem nhiều nhất
+//------------------------------------------------------------------------------
+
+$lang['module_name_mostvisited'] = 'Được xem nhiều nhất';
+$lang['module_desc_mostvisited'] = 'Hiển thị những ảnh được xem nhiều nhất trong thư viện hay trong một đề mục';
+
+$lang['stuffs_mv_nb_images'] = 'Số hình tối đa được hiển thị';
+$lang['stuffs_mv_category_display'] = 'Hiển thị trên các trang đề mục';
+$lang['stuffs_mv_all_cats'] = 'Tất cả các đề mục';
+$lang['stuffs_mv_w_thumb'] = 'Chỉ những đề mục có hình thu nhỏ';
+$lang['stuffs_mv_wo_thumb'] = 'Chỉ những đề mục không có hình thu nhỏ';
+$lang['stuffs_mv_selected_cats'] = 'Chỉ những đề mục đã chọn';
+
+$lang['stuffs_mv_cat_selection'] = 'Chọn lựa đề mục';
+$lang['stuffs_mv_hold_ctrl'] = 'giữ phím CTRL';
+
+//------------------------------------------------------------------------------
+//              Module Xếp hạng cao nhất
+//------------------------------------------------------------------------------
+
+$lang['module_name_bestrated'] = 'Xếp hạng cao nhất';
+$lang['module_desc_bestrated'] = 'Hiển thị những ảnh được xếp hạng cao nhất trong thư viện hoặc trong một đề mục';
+
+//------------------------------------------------------------------------------
+//              Module Gần đây
+//------------------------------------------------------------------------------
+
+$lang['module_name_recent'] = 'Những ảnh gần đây';
+$lang['module_desc_recent'] = 'Hiển thị những ảnh gần đây trong thư viện hoặc trong một đề mục (nếu có)';
+
+//------------------------------------------------------------------------------
+//              Module Ngẫu nhiên
+//------------------------------------------------------------------------------
+
+$lang['module_name_random'] = 'Ảnh ngẫu nhiên';
+$lang['module_desc_random'] = 'Hiển thị những ảnh ngẫu nhiên trong thư viện hoặc trong một đề mục';
+
+//------------------------------------------------------------------------------
+//              Module Trích dẫn
+//------------------------------------------------------------------------------
+
+$lang['module_name_citation'] = 'Trích dẫn ngẫu nhiên';
+$lang['module_desc_citation'] = 'Hiện thị một trích dẫn ngẫu nhiên';
+$lang['module_Citation_lang'] = 'Ngôn ngữ';
+$lang['module_Citation_code'] = 'Mã liên quan';
+
+//------------------------------------------------------------------------------
+//              Module Ảnh đặc trưng
+//------------------------------------------------------------------------------
+
+$lang['module_name_featuredphotos'] = 'Ảnh đặc trưng';
+$lang['module_desc_featuredphotos'] = 'Hiển thị ảnh ưa thích của webmaster';
+
+?>
