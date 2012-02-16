6daadd15ca7a8a1c9a987422bf48714345fa7e92
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-16 12:10:51 +0000

    [PWG Stuffs] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@13188 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,110 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Hide on home page'] = 'Приховати на домашній сторінці';
+$lang['lastcoms_config'] = 'Конфігурація';
+$lang['lastcoms_dim'] = 'Розмір мініатюри';
+$lang['lastcoms_height'] = 'Максимальна висота';
+$lang['lastcoms_nb_cadres'] = 'Кількість кадрів по лінії';
+$lang['lastcoms_nb_comments'] = 'Показано кількість коментарів';
+$lang['lastcoms_no_nb_comments'] = 'Ви повинні вказати кількість коментарів.';
+$lang['lastcoms_show_buttons'] = 'Показати кнопки адміністратора';
+$lang['lastcoms_taille_cadres'] = 'Висота кадрів';
+$lang['lastcoms_width'] = 'Максимальна ширина';
+$lang['logon_remove_menubar_block'] = 'Видалити "Швидке з\'єднання" блока з меню';
+$lang['module_Citation_code'] = 'Пов\'язаний код';
+$lang['module_Citation_lang'] = 'Мова';
+$lang['module_desc_bestrated'] = 'Показати найкраще оцінені в галереї або категорії';
+$lang['module_desc_citation'] = 'Показати випадкову цитату';
+$lang['module_desc_featuredphotos'] = 'Показати обрані фотографії веб-майстра';
+$lang['module_desc_lastcoms'] = 'Показати останні розміщені коментарі';
+$lang['module_desc_logon'] = 'Показати блок входу';
+$lang['module_desc_mostvisited'] = 'Показати найбільш відвідуваних картин в галереї або категорії';
+$lang['module_desc_personal'] = 'Показати персональний блок (як редактор)';
+$lang['module_desc_random'] = 'Відображення випадкових фотографій в галереї або категорії';
+$lang['module_desc_recent'] = 'Відображення останніх фотографій в галереї або категорії (якщо є)';
+$lang['module_desc_tags'] = 'Показати теґи галереї';
+$lang['module_name_bestrated'] = 'Найкраща оцінка';
+$lang['module_name_citation'] = 'Випадкова цитата';
+$lang['module_name_featuredphotos'] = 'Краще фото';
+$lang['module_name_lastcoms'] = 'Останні коментарі';
+$lang['module_name_logon'] = 'Увійти';
+$lang['module_name_mostvisited'] = 'Найбільш відвідувані';
+$lang['module_name_personal'] = 'Персональний блок';
+$lang['module_name_random'] = 'Випадкові фотографії';
+$lang['module_name_recent'] = 'Нові фотографії';
+$lang['module_name_tags'] = 'Теґи';
+$lang['personal_content'] = 'зміст';
+$lang['stuffs_action'] = 'Дія';
+$lang['stuffs_add_mod'] = 'Додати блок';
+$lang['stuffs_add_module'] = 'Додати новий блок';
+$lang['stuffs_authorized_group'] = 'Авторизовані групи';
+$lang['stuffs_authorized_users'] = 'Авторизовані користувачі';
+$lang['stuffs_confirm_delete'] = 'Ви дійсно хочете видалити блок %s?';
+$lang['stuffs_delete'] = 'Видалити блок';
+$lang['stuffs_desc'] = 'Опис';
+$lang['stuffs_edit'] = 'Змінити блок';
+$lang['stuffs_edit_mod'] = 'Редагувати блок';
+$lang['stuffs_error_no_mod_selected'] = 'Ви не вибрали жодного блоку.';
+$lang['stuffs_group_perm'] = 'за групами';
+$lang['stuffs_id_ligne'] = 'ID лінія';
+$lang['stuffs_level_perm'] = 'за рівнем конфіденційності';
+$lang['stuffs_main_block'] = 'Основний блок';
+$lang['stuffs_mods_management'] = 'Управління блоками';
+$lang['stuffs_module_config'] = 'Конфігурація блоку';
+$lang['stuffs_module_options'] = 'Варіанти блоків';
+$lang['stuffs_module_title'] = 'Назва блоку';
+$lang['stuffs_mv_all_cats'] = 'Всі категорії';
+$lang['stuffs_mv_category_display'] = 'Показати за категоріями сторінок';
+$lang['stuffs_mv_cat_selection'] = 'Категорії відбору';
+$lang['stuffs_mv_hold_ctrl'] = 'утримуйте клавішу CTRL';
+$lang['stuffs_mv_nb_images'] = 'Максимальна кількість фотографій для показу';
+$lang['stuffs_mv_selected_cats'] = 'Тільки вибоані категорії';
+$lang['stuffs_mv_wo_thumb'] = 'Тільки категорій без мініатюр';
+$lang['stuffs_mv_w_thumb'] = 'Тільки категорій з мініатюрами';
+$lang['stuffs_no_name'] = 'Ви повинні вказати назву блоку';
+$lang['stuffs_no_options'] = 'Немає варіантів для цього блоку';
+$lang['stuffs_only_for_admin'] = '(тільки для адміністративної панелі)';
+$lang['stuffs_order_saved'] = 'Порядок успішно збережено.';
+$lang['stuffs_parent_plugin_is_missing'] = 'БАТЬКІВСЬКИЙ ПЛАҐІН НЕ АКТИВОВАНИЙ. Ви повинні видалити цей блок або повторно активувати батьківський плаґін.';
+$lang['stuffs_perm'] = 'Увімкніть дозвіл управління:';
+$lang['stuffs_piclens_need_upgrade'] = 'PICLENS ПЛАҐІН ПОВИНЕН БУТИ ОНОВЛЕНИЙ. PWG Stuffs не сумісний з цією версією PICLENS.';
+$lang['stuffs_position'] = 'Позиція';
+$lang['stuffs_public_module'] = '(Не порожній для публічного блоку)';
+$lang['stuffs_select_all'] = 'Вибрати все';
+$lang['stuffs_show_module_on'] = 'Показати блок :';
+$lang['stuffs_show_on_cats'] = 'На всіх сторінках категорій';
+$lang['stuffs_show_on_home'] = 'На доманій сторінці';
+$lang['stuffs_show_on_picture'] = 'На сторінці фотографії';
+$lang['stuffs_show_on_root'] = 'На сторінці кореневої категорії';
+$lang['stuffs_show_title'] = 'Показати назву блоку';
+$lang['stuffs_tags_cloud'] = 'Хмара теґів';
+$lang['stuffs_tags_cumulus'] = 'Використовуйте режим безмежності (плаґін <a href=http://piwigo.org/ext/extension_view.php?eid=263 target="_blank">Cumulus Tags Cloud</a> повинна бути встановлена​​)';
+$lang['stuffs_tags_default_display'] = 'Показати теґи';
+$lang['stuffs_tags_letters'] = 'Групувати по буквам';
+$lang['stuffs_type'] = 'Тип блоку';
+$lang['stuffs_unselect_all'] = 'Скасувати вибір';
+$lang['stuffs_upgrade_message'] = 'PWG Stuffs плаґін був перейменований. Тепер він називається «блоки».';
+$lang['stuffs_user_perm'] = 'за типами користувачів';
+$lang['stuffs_visual_config'] = 'Відображення конфігурації';
+$lang['stuffs_width'] = 'Ширина';
+?>
