3f51b3e0f5830bd526ba1f7924fb3964030dce04
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-03-28 20:06:52 +0000

    [PWG Stuffs] Add sh_RS, thanks to : vladamilanovic
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@13791 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sh_RS/plugin.lang.php
@@ -0,0 +1,63 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Hide on home page'] = 'Sakrij na početnoj strani';
+$lang['stuffs_action'] = 'Akcije';
+$lang['stuffs_add_mod'] = 'Dodaj blokadu';
+$lang['stuffs_add_module'] = 'Dodaj novu blokadu';
+$lang['stuffs_authorized_group'] = 'Autorizovane grupe';
+$lang['stuffs_authorized_users'] = 'Autorizovani korisnici';
+$lang['stuffs_confirm_delete'] = 'Da li ste sigurni da želite da obrišete blokadu %s??';
+$lang['stuffs_delete'] = 'Obriši blokadu';
+$lang['stuffs_desc'] = 'Opis';
+$lang['stuffs_edit'] = 'Promeni blokadu';
+$lang['stuffs_edit_mod'] = 'Izmeni blokadu';
+$lang['stuffs_error_no_mod_selected'] = 'Niste odabrali ni jednu blokadu.';
+$lang['stuffs_group_perm'] = 'po grupama';
+$lang['stuffs_id_ligne'] = 'ID linija';
+$lang['stuffs_level_perm'] = 'po nivou privatnosti';
+$lang['stuffs_main_block'] = 'Glavna blokada';
+$lang['stuffs_mods_management'] = 'Blokada upravljanja';
+$lang['stuffs_module_config'] = 'Blokiraj podešavanja';
+$lang['stuffs_module_options'] = 'Blokiraj opcije';
+$lang['stuffs_module_title'] = 'Blokiraj naslov';
+$lang['stuffs_no_name'] = 'Morate upisati naziv blokade';
+$lang['stuffs_no_options'] = 'Nema opcija za ovu blokadu';
+$lang['stuffs_only_for_admin'] = '(samo za administracioni panel)';
+$lang['stuffs_order_saved'] = 'Poredak uspešno sačuvan.';
+$lang['stuffs_perm'] = 'Uključi upravljanje autorizacije';
+$lang['stuffs_position'] = 'Pozicija';
+$lang['stuffs_public_module'] = '(Ostavite prazno za javnu blokadu)';
+$lang['stuffs_select_all'] = 'Odaberi sve';
+$lang['stuffs_show_module_on'] = 'Prikaži blokadu:';
+$lang['stuffs_show_on_cats'] = 'Na svim stranama kategorija';
+$lang['stuffs_show_on_home'] = 'Na glavnoj strani';
+$lang['stuffs_show_on_picture'] = 'Na strani sa slikama';
+$lang['stuffs_show_on_root'] = 'Na korenoj strani kategorija';
+$lang['stuffs_show_title'] = 'Prikaži naziv blokade';
+$lang['stuffs_type'] = 'Tip blokade';
+$lang['stuffs_unselect_all'] = 'Poništi odabir';
+$lang['stuffs_upgrade_message'] = 'PWG Stuffs dodatak je promenio naziv. Sada se naziva "Blokade".';
+$lang['stuffs_user_perm'] = 'po tipu korisnika';
+$lang['stuffs_visual_config'] = 'Prikaži podešavanja';
+$lang['stuffs_width'] = 'Širina';
+?>
