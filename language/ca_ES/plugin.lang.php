edc1ba372df211673ead62d2df530635cfaa04ee
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-26 15:17:33 +0000

    [pwg_stuffs] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@14446 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['stuffs_action'] = 'Accions';
+$lang['stuffs_add_mod'] = 'Afegir un bloc';
+$lang['stuffs_confirm_delete'] = 'Estàs segur que desitjes eliminar el bloc %s?';
+$lang['stuffs_delete'] = 'Eliminar bloc';
+$lang['stuffs_desc'] = 'Descripció';
+$lang['stuffs_edit_mod'] = 'Editar bloc';
+$lang['stuffs_edit'] = 'Modificar bloc';
+$lang['stuffs_id_ligne'] = 'Línia ID';
+$lang['stuffs_mods_management'] = 'Gestió de blocs';
+$lang['stuffs_module_title'] = 'Títol del bloc';
+$lang['stuffs_only_for_admin'] = '(Només per al panell d\'administració)';
+$lang['stuffs_position'] = 'Posició';
+$lang['stuffs_type'] = 'Tipus de bloc';
+$lang['stuffs_width'] = 'Amplada';
+?>
