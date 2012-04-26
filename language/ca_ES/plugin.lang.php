2d5ad86bed0e4414a2746bad476834088aea9cf5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-26 19:46:11 +0000

    [pwg_stuffs] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@14453 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ca_ES/plugin.lang.php
+++ language/ca_ES/plugin.lang.php
@@ -34,4 +34,107 @@ $lang['stuffs_only_for_admin'] = '(Només per al panell d\'administració)';
 $lang['stuffs_position'] = 'Posició';
 $lang['stuffs_type'] = 'Tipus de bloc';
 $lang['stuffs_width'] = 'Amplada';
+$lang['Hide on home page'] = 'Ocultar a la pàgina principal';
+$lang['lastcoms_config'] = 'Configuració';
+$lang['lastcoms_dim'] = 'Mida de les miniatures';
+$lang['lastcoms_height'] = 'Alçada màxima';
+$lang['lastcoms_nb_cadres'] = 'Número de fotogrames per línia';
+$lang['lastcoms_nb_comments'] = 'Número de comentaris a visualitzar';
+$lang['lastcoms_no_nb_comments'] = 'Heu d\'especificar el número de comentaris.';
+$lang['lastcoms_show_buttons'] = 'Mostrar els botons d\'administració';
+$lang['lastcoms_taille_cadres'] = 'Alçada del fotograma';
+$lang['lastcoms_width'] = 'Amplada màxima';
+$lang['logon_remove_menubar_block'] = 'Eliminar el bloc "Connexió ràpida" de la barra de menú';
+$lang['module_Citation_code'] = 'Codi relacionat';
+$lang['module_Citation_lang'] = 'Idioma';
+$lang['module_desc_bestrated'] = 'Mostra x imatges més ben valorades a la galeria o a una categoria';
+$lang['module_desc_citation'] = 'Visualitzar cites aleatòriament';
+$lang['module_desc_featuredphotos'] = 'Mostra les imatges preferides del webmaster';
+$lang['module_desc_lastcoms'] = 'Mostra els últims comentaris publicats';
+$lang['module_desc_logon'] = 'Visualitzar el bloc d\'inici de sessió';
+$lang['module_desc_mostvisited'] = 'Mostra x imatges més visitades a la galeria o a una categoria';
+$lang['module_desc_personal'] = 'Mostra el bloc personal (com un edicte)';
+$lang['module_desc_random'] = 'Mostra x imatges aleatòries a la galeria o a una categoria';
+$lang['module_desc_recent'] = 'Mostra x imatges més recents a la galeria o a una categoria (si existeix)';
+$lang['module_desc_tags'] = 'Mostrar la galeria d\'etiquetes';
+$lang['module_name_bestrated'] = 'Amb millor valocació';
+$lang['module_name_citation'] = 'Citar comentaris aleatòriament';
+$lang['module_name_featuredphotos'] = 'Imatges destacades';
+$lang['module_name_lastcoms'] = 'Últims comentaris';
+$lang['module_name_logon'] = 'Inscriu-te';
+$lang['module_name_mostvisited'] = 'El més visitat';
+$lang['module_name_personal'] = 'Bloc personal';
+$lang['module_name_random'] = 'Imatges a l\'atzar';
+$lang['module_name_recent'] = 'Imatges més recents';
+$lang['module_name_tags'] = 'Etiquetes';
+$lang['personal_content'] = 'contingut';
+$lang['stuffs_add_module'] = 'Afegir un nou bloc';
+$lang['stuffs_authorized_group'] = 'Grups autoritzats';
+$lang['stuffs_authorized_users'] = 'Usuaris autoritzats';
+$lang['stuffs_error_no_mod_selected'] = 'No heu seleccionat cap bloc.';
+$lang['stuffs_group_perm'] = 'per grups';
+$lang['stuffs_level_perm'] = 'per nivell de privacitat';
+$lang['stuffs_main_block'] = 'Bloc principal';
+$lang['stuffs_module_config'] = 'Configuració del bloc';
+$lang['stuffs_module_options'] = 'Opcions del bloc';
+$lang['stuffs_mv_all_cats'] = 'Totes les categories';
+$lang['stuffs_mv_cat_selection'] = 'Selecció de categories';
+$lang['stuffs_mv_category_display'] = 'Mostra a les pàgines de categories';
+$lang['stuffs_mv_hold_ctrl'] = 'tecla CTRL';
+$lang['stuffs_mv_nb_images'] = 'Número màxim d\'imatges a mostrar';
+$lang['stuffs_mv_selected_cats'] = 'Només les categories seleccionades';
+$lang['stuffs_mv_w_thumb'] = 'Només les categories amb miniatures';
+$lang['stuffs_mv_wo_thumb'] = 'Només les categories sense miniatures';
+$lang['stuffs_no_name'] = 'Has d\'especificar el nom del bloc';
+$lang['stuffs_no_options'] = 'No hi ha opcions per a aquest bloc';
+$lang['stuffs_order_saved'] = 'Ordre guardat correctament.';
+$lang['stuffs_parent_plugin_is_missing'] = 'El complement Parents no està activat. Has d\'eliminar aquest bloc o tornar a reactivar el plugin.';
+$lang['stuffs_perm'] = 'Activar la gestió de l\'autorització:';
+$lang['stuffs_piclens_need_upgrade'] = 'El complement PicLens ha de ser actualitzat. PWG Stuffs no és compatible amb aquesta versió de Piclens.';
+$lang['stuffs_public_module'] = '(Mantindre-ho buit per als blocs públics)';
+$lang['stuffs_select_all'] = 'Selecciona-ho tot';
+$lang['stuffs_show_module_on'] = 'Mostrar el bloc:';
+$lang['stuffs_show_on_cats'] = 'En totes les pàgines de categories';
+$lang['stuffs_show_on_home'] = 'A la pàgina principal';
+$lang['stuffs_show_on_picture'] = 'En les pàgines d\'imatges';
+$lang['stuffs_show_on_root'] = 'A la pàgina arrel de les categories';
+$lang['stuffs_show_title'] = 'Mostra el títol del bloc';
+$lang['stuffs_tags_cloud'] = 'Núbol d\'etiquetes';
+$lang['stuffs_tags_cumulus'] = '
+
+
+
+
+
+
+
+
+
+
+
+
+
+español
+
+
+
+catalán
+
+
+
+francés
+
+
+
+
+
+
+
+Utilitza el mode de cumulus (el complement<a href=http://piwigo.org/ext/extension_view.php?eid=263 target="_blank"> Cumulus Tags Cloud</a> ha d\'estar instal·lat)';
+$lang['stuffs_tags_default_display'] = 'Visualitzar etiquetes';
+$lang['stuffs_tags_letters'] = 'Agrupar per lletres';
+$lang['stuffs_unselect_all'] = 'Deselecciona-ho tot';
+$lang['stuffs_upgrade_message'] = 'El complement PWG Stuffs s\'ha canviat el nom. Ara es diu "blocs".';
+$lang['stuffs_user_perm'] = 'per tipus d\'usuari';
+$lang['stuffs_visual_config'] = 'Visualitzar la configuració';
 ?>
