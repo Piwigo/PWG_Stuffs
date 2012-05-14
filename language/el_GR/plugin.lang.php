002916532967faf9703b50a28a6272d2dc648900
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-14 04:58:27 +0000

    [pwg_stuffs] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@14968 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Hide on home page'] = 'Απόκρυψη στην αρχική σελίδα';
+$lang['module_name_lastcoms'] = 'Τελευταία σχόλια';
+$lang['stuffs_action'] = 'Δράσεις';
+$lang['stuffs_add_mod'] = 'Προσθέστε ένα μπλοκ';
+$lang['stuffs_add_module'] = 'Προσθήκη νέου μπλοκ';
+$lang['stuffs_authorized_group'] = 'Εξουσιοδοτημένες ομάδες';
+$lang['stuffs_authorized_users'] = 'Εξουσιοδοτημένοι χρήστες';
+$lang['stuffs_confirm_delete'] = 'Είστε βέβαιοι ότι θέλετε να διαγράψετε το μπλοκ %s;';
+$lang['stuffs_delete'] = 'Διαγραφή μπλοκ';
+$lang['stuffs_desc'] = 'Περιγραφή';
+$lang['stuffs_edit_mod'] = 'Επεξεργασία μπλοκ';
+$lang['stuffs_edit'] = 'Τροποποίηση μπλοκ';
+$lang['stuffs_error_no_mod_selected'] = 'Δεν έχετε επιλέξει κανένα μπλοκ.';
+$lang['stuffs_group_perm'] = 'κατά ομάδες';
+$lang['stuffs_id_ligne'] = 'ID γραμμή';
+$lang['stuffs_level_perm'] = 'από το επίπεδο της ιδιωτικότητας';
+$lang['stuffs_main_block'] = 'Το Κύριο μπλοκ';
+$lang['stuffs_mods_management'] = 'Διαχείρηση Μπλοκ';
+$lang['stuffs_module_config'] = 'Διαμόρφωση μπλοκ';
+$lang['stuffs_module_options'] = 'Επιλογές μπλοκ';
+$lang['stuffs_module_title'] = 'Τίτλος του μπλοκ';
+$lang['stuffs_no_name'] = 'Πρέπει να καθορίσετε το όνομα του μπλοκ';
+$lang['stuffs_no_options'] = 'Δεν υπάρχουν επιλογές για αυτό το μπλοκ';
+$lang['stuffs_only_for_admin'] = '(μόνο για το pannel διαχείρισης)';
+$lang['stuffs_order_saved'] = 'Η κατάταξη αποθηκεύτηκε με επιτυχία.';
+$lang['stuffs_parent_plugin_is_missing'] = 'ΤΟ ΜΗΤΡΙΚΟ ΠΡΟΣΘΕΤΟ ΔΕΝ ΕΙΝΑΙ ΕΝΕΡΓΟΠΟΙΗΜΕΝΟ. Θα πρέπει να αφαιρέσετε αυτό το μπλοκ ή να επαν-ενεργοποίησετε το μητρικό πρόσθετο.';
+$lang['stuffs_perm'] = 'Ενεργοποιήστε τη διαχείριση αδειών:';
+$lang['stuffs_piclens_need_upgrade'] = 'Το πρόσθετο PicLens πρέπει να ενημερωθεί. Το PWG Stuffs δεν είναι συμβατό με αυτήν την έκδοση του PicLens.';
+$lang['stuffs_position'] = 'Θέση';
+$lang['stuffs_public_module'] = '(Κρατήστε κενό για δημόσια μπλοκ)';
+$lang['stuffs_select_all'] = 'Επιλογή όλων';
+$lang['stuffs_show_module_on'] = 'Εμφάνιση μπλοκ:';
+$lang['stuffs_show_on_cats'] = 'Σε όλες τις σελίδες κατηγοριών';
+$lang['stuffs_show_on_home'] = 'Στην κεντρική σελίδα';
+$lang['stuffs_show_on_picture'] = 'Στις σελίδες εικόνων';
+$lang['stuffs_show_on_root'] = 'Στις κεντρική σελίδα κατηγοριών';
+$lang['stuffs_show_title'] = 'Εμφάνιση τίτλου μπλοκ';
+$lang['stuffs_type'] = 'Τύπος Block';
+$lang['stuffs_unselect_all'] = 'Αποεπιλογή όλων';
+$lang['stuffs_upgrade_message'] = 'PWG Stuffs πρόσθετο, όπως έχει μετονομαστεί. Ονομάζεται πλέον «Μπλοκ».';
+$lang['stuffs_user_perm'] = 'κατα τύπο χρήστη';
+$lang['stuffs_visual_config'] = 'Εμφάνιση διαμόρφωσης';
+$lang['stuffs_width'] = 'Πλάτος';
+?>
