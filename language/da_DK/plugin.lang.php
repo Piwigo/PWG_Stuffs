f434ae545c0f078643a9541ac858541ba0393c4d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-19 22:11:28 +0000

    [pwg_stuffs] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@18060 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,110 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['stuffs_tags_letters'] = 'Gruppér efter bogstaver';
+$lang['stuffs_type'] = 'Bloktype';
+$lang['stuffs_unselect_all'] = 'Fravælg alle';
+$lang['stuffs_upgrade_message'] = 'Plugin\'en PWG Stuffs har fået nyt navn. Den hedder nu "Blocks".';
+$lang['stuffs_user_perm'] = 'efter brugertype';
+$lang['stuffs_visual_config'] = 'Vis opsætning';
+$lang['stuffs_width'] = 'Bredde';
+$lang['stuffs_mv_wo_thumb'] = 'Kun kategorier uden miniaturebilleder';
+$lang['stuffs_no_name'] = 'Du skal angive blokkens navn.';
+$lang['stuffs_no_options'] = 'Ingen valgmuligheder i denne blok';
+$lang['stuffs_only_for_admin'] = '(kun til administrationspanelet)';
+$lang['stuffs_order_saved'] = 'Rækkefølge gemt.';
+$lang['stuffs_parent_plugin_is_missing'] = 'OVERORDNET PLUGIN ER IKKE AKTIVERET. Du bør fjerne denne blok eller genaktivere den overordnede plugin.';
+$lang['stuffs_perm'] = 'Aktiver autorisationsadministration';
+$lang['stuffs_piclens_need_upgrade'] = 'PICLENS-PLUGIN\'EN SKAL OPDATERES. PWG Stuffs er ikke kompatibel med denne version af Piclens.';
+$lang['stuffs_position'] = 'Position';
+$lang['stuffs_public_module'] = '(Hold tom ved offentlige blokke)';
+$lang['stuffs_select_all'] = 'Vælg alle';
+$lang['stuffs_show_module_on'] = 'Vis blok:';
+$lang['stuffs_show_on_cats'] = 'På alle kategorisider';
+$lang['stuffs_show_on_home'] = 'På forsiden';
+$lang['stuffs_show_on_picture'] = 'På billedsider';
+$lang['stuffs_show_on_root'] = 'På kategoriernes rodside';
+$lang['stuffs_show_title'] = 'Vis bloktitel';
+$lang['stuffs_tags_cloud'] = 'Tag-sky';
+$lang['stuffs_tags_cumulus'] = 'Anvend kumulustilstand (plugin\'en <a href=http://piwigo.org/ext/extension_view.php?eid=263 target="_blank">Cumulus Tags Cloud</a> skal installeres)';
+$lang['stuffs_tags_default_display'] = 'Tagvisning';
+$lang['stuffs_delete'] = 'Slet blok';
+$lang['stuffs_desc'] = 'Beskrivelse';
+$lang['stuffs_edit'] = 'Ændr blok';
+$lang['stuffs_edit_mod'] = 'Rediger blok';
+$lang['stuffs_error_no_mod_selected'] = 'Du har ikke valgt en blok.';
+$lang['stuffs_group_perm'] = 'efter grupper';
+$lang['stuffs_id_ligne'] = 'Id-linje';
+$lang['stuffs_level_perm'] = 'efter privatlivsniveau';
+$lang['stuffs_main_block'] = 'Hovedblok';
+$lang['stuffs_mods_management'] = 'Blokadministration';
+$lang['stuffs_module_config'] = 'Blokopsætning';
+$lang['stuffs_module_options'] = 'Blokvalgmuligheder';
+$lang['stuffs_module_title'] = 'Bloktitel';
+$lang['stuffs_mv_all_cats'] = 'Alle kategorier';
+$lang['stuffs_mv_cat_selection'] = 'Kategorivalg';
+$lang['stuffs_mv_category_display'] = 'Vis på kategorisiden';
+$lang['stuffs_mv_hold_ctrl'] = 'tryk på CTRL';
+$lang['stuffs_mv_nb_images'] = 'Maksimalt antal billeder der skal vises';
+$lang['stuffs_mv_selected_cats'] = 'Kun valgte kategorier';
+$lang['stuffs_mv_w_thumb'] = 'Kun kategorier med miniaturebilleder';
+$lang['module_desc_random'] = 'Vis x tilfældige billeder i galleriet eller i et album';
+$lang['module_desc_recent'] = 'Vis x nye billeder i galleriet eller i et album';
+$lang['module_desc_tags'] = 'Vis galleritags';
+$lang['module_name_bestrated'] = 'Bedst bedømte';
+$lang['module_name_citation'] = 'Tilfældigt citat';
+$lang['module_name_featuredphotos'] = 'Udvalgte fotografier';
+$lang['module_name_lastcoms'] = 'Seneste kommentarer';
+$lang['module_name_logon'] = 'Login';
+$lang['module_name_mostvisited'] = 'Mest besøgte';
+$lang['module_name_personal'] = 'Personlig blok';
+$lang['module_name_random'] = 'Tilfældige billeder';
+$lang['module_name_recent'] = 'Nye billeder';
+$lang['module_name_tags'] = 'Tags';
+$lang['personal_content'] = 'indhold';
+$lang['stuffs_action'] = 'Handlinger';
+$lang['stuffs_add_mod'] = 'Tilføj en blok';
+$lang['stuffs_add_module'] = 'Tilføj ny blok';
+$lang['stuffs_authorized_group'] = 'Autoriserede grupper';
+$lang['stuffs_authorized_users'] = 'Autoriserede brugere';
+$lang['stuffs_confirm_delete'] = 'Er du sikker på, at du vil slette blokken %s?';
+$lang['Hide on home page'] = 'Skjul på forsiden';
+$lang['lastcoms_config'] = 'Opsætning';
+$lang['lastcoms_dim'] = 'Størrelse på miniaturebilleder';
+$lang['lastcoms_height'] = 'Maksimal højde';
+$lang['lastcoms_nb_cadres'] = 'Antallet af rammer pr. linje';
+$lang['lastcoms_nb_comments'] = 'Antallet af kommentarer der skal vises';
+$lang['lastcoms_no_nb_comments'] = 'Du skal angive antal kommentarer.';
+$lang['lastcoms_show_buttons'] = 'Vis admin-knapper';
+$lang['lastcoms_taille_cadres'] = 'Rammehøjde';
+$lang['lastcoms_width'] = 'Maksimal bredde';
+$lang['logon_remove_menubar_block'] = 'Fjern "Hurtig login"-blokken fra menubjælken';
+$lang['module_Citation_code'] = 'Relateret kode';
+$lang['module_Citation_lang'] = 'Sprog';
+$lang['module_desc_bestrated'] = 'Vis x bedst bedømte billeder i galleriet eller albummet';
+$lang['module_desc_citation'] = 'Vis et tilfældigt citat';
+$lang['module_desc_featuredphotos'] = 'Vis webmasters yndlingsfotografier';
+$lang['module_desc_lastcoms'] = 'Vis de seneste kommentarer';
+$lang['module_desc_logon'] = 'Vis loginblok';
+$lang['module_desc_mostvisited'] = 'Vis x mest sete billeder i galleriet eller albummet';
+$lang['module_desc_personal'] = 'Vis personlig blok (som en editor)';
+?>
