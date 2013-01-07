0fa9daaa9540347db7a3e49313fa357dffb342eb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-07 21:41:00 +0000

    [pwg_stuffs] Add pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@19967 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,110 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['stuffs_tags_cumulus'] = 'Use o modo cumulus (plugin <a href=http://piwigo.org/ext/extension_view.php?eid=263 target="_blank">Cumulus Tags Cloud</a> deve ser instalado)';
+$lang['stuffs_order_saved'] = 'Ordenamento salvo com sucesso.';
+$lang['stuffs_parent_plugin_is_missing'] = 'PLUGIN PAI NÃO ESTÁ ATIVADO. Você deve remover esse bloqueio ou reativar o plugin pai.';
+$lang['stuffs_perm'] = 'Gerenciamento de autorização ativado';
+$lang['stuffs_piclens_need_upgrade'] = 'PLUGUIN PICLENS DEVE SER ATUALIZADO. PWD Stuff não é compatível com essa versão do Piclens.';
+$lang['stuffs_tags_cloud'] = 'Nuvem de etiquetas';
+$lang['stuffs_tags_default_display'] = 'Mostra etiquetas';
+$lang['stuffs_tags_letters'] = 'Agrupe por letras';
+$lang['stuffs_type'] = 'Tipo de bloqueio';
+$lang['stuffs_unselect_all'] = 'Desmarque todos';
+$lang['stuffs_upgrade_message'] = 'Plugin PWG Stuff foi renomeado. Ele agora se chama "Blocks".';
+$lang['stuffs_user_perm'] = 'por tipo de usuário';
+$lang['stuffs_visual_config'] = 'Mostra configuração';
+$lang['stuffs_width'] = 'Largura';
+$lang['stuffs_main_block'] = 'Bloqueio principal';
+$lang['stuffs_mods_management'] = 'Gerenciar bloqueios';
+$lang['stuffs_module_config'] = 'Configuração de bloqueios';
+$lang['stuffs_module_options'] = 'Opções de bloqueio';
+$lang['stuffs_no_name'] = 'Você deve especificar um nome para o bloqueio';
+$lang['stuffs_no_options'] = 'Nenhuma opção para esse bloqueio';
+$lang['stuffs_only_for_admin'] = '(somente para o painel administrativo)';
+$lang['stuffs_position'] = 'Posição';
+$lang['stuffs_public_module'] = '(Deixe vazio para bloqueio público)';
+$lang['stuffs_select_all'] = 'Selecione todos';
+$lang['stuffs_show_module_on'] = 'Mostre bloqueio';
+$lang['stuffs_show_on_cats'] = 'Em todas as páginas de categorias';
+$lang['stuffs_show_on_home'] = 'Na página principal';
+$lang['stuffs_show_on_picture'] = 'Na página de fotos';
+$lang['stuffs_show_on_root'] = 'Ná página da categoria pai';
+$lang['stuffs_show_title'] = 'Mostra título do bloqueio';
+$lang['stuffs_desc'] = 'Descrição';
+$lang['stuffs_edit'] = 'Modificar bloqueio';
+$lang['stuffs_edit_mod'] = 'Editar bloqueio';
+$lang['stuffs_error_no_mod_selected'] = 'Você não selecionou nenhum bloqueio';
+$lang['stuffs_group_perm'] = 'por grupos';
+$lang['stuffs_id_ligne'] = 'linha ID';
+$lang['stuffs_level_perm'] = 'por nível de privacidade';
+$lang['stuffs_module_title'] = 'Título do bloqueio';
+$lang['stuffs_mv_all_cats'] = 'Todas as categorias';
+$lang['stuffs_mv_cat_selection'] = 'Seleção de categorias';
+$lang['stuffs_mv_category_display'] = 'Mostra na Página de categorias';
+$lang['stuffs_mv_hold_ctrl'] = 'segure CTRL';
+$lang['stuffs_mv_nb_images'] = 'Número máximo de fotos para mostrar';
+$lang['stuffs_mv_selected_cats'] = 'Somente categorias selecionadas';
+$lang['stuffs_mv_w_thumb'] = 'Somente categorias com miniaturas';
+$lang['stuffs_mv_wo_thumb'] = 'Somente categorias sem miniaturas';
+$lang['module_desc_recent'] = 'Mostra x fotos recentes na galeria ou numa categoria (se existir)';
+$lang['module_desc_tags'] = 'Mostra etiquetas da galeria';
+$lang['module_name_bestrated'] = 'Melhor avaliado';
+$lang['module_name_citation'] = 'quote aleatório';
+$lang['module_name_featuredphotos'] = 'Fotos Destacadas';
+$lang['module_name_lastcoms'] = 'últimos comentários';
+$lang['module_name_logon'] = 'Autenticar';
+$lang['module_name_mostvisited'] = 'Mais visitados';
+$lang['module_name_personal'] = 'Bloqueio Pessoal';
+$lang['module_name_random'] = 'Fotos aleatórias';
+$lang['module_name_recent'] = 'Fotos recentes';
+$lang['module_name_tags'] = 'Etiquetas';
+$lang['personal_content'] = 'conteúdo';
+$lang['stuffs_action'] = 'Ações';
+$lang['stuffs_add_mod'] = 'adicione um bloqueio';
+$lang['stuffs_add_module'] = 'adicione um novo bloqueio';
+$lang['stuffs_authorized_group'] = 'Grupos autorizados';
+$lang['stuffs_authorized_users'] = 'Usuários autorizados';
+$lang['stuffs_confirm_delete'] = 'Você tem certeza que quer apagar o bloqueio %s?';
+$lang['stuffs_delete'] = 'Apagar bloqueio';
+$lang['lastcoms_show_buttons'] = 'Mostre botoões adminstrativos';
+$lang['lastcoms_taille_cadres'] = 'Altura do quadro';
+$lang['lastcoms_width'] = 'Máxima largura';
+$lang['logon_remove_menubar_block'] = 'Retire o bloco "conexão rápida" do menu';
+$lang['module_Citation_code'] = 'Código relacionado';
+$lang['module_desc_bestrated'] = 'Mostra x fotos melhor avaliadas na galeria ou numa categoria';
+$lang['module_desc_random'] = 'Mostra x fotos aleatórias na galeria ou  numa categoria';
+$lang['Hide on home page'] = 'Esconde a página principal';
+$lang['lastcoms_config'] = 'Configurações';
+$lang['lastcoms_dim'] = 'Tamanho das miniaturas';
+$lang['lastcoms_height'] = 'Altura máxima';
+$lang['lastcoms_nb_cadres'] = 'Número de quadros por linha';
+$lang['lastcoms_nb_comments'] = 'Número de comentários para mostrar';
+$lang['lastcoms_no_nb_comments'] = 'Você deve especificar o número de comentários.';
+$lang['module_Citation_lang'] = 'idioma';
+$lang['module_desc_citation'] = 'Mostra quote aleatório';
+$lang['module_desc_featuredphotos'] = 'Mostra fotos favoritas do Webmaster';
+$lang['module_desc_lastcoms'] = 'Mostra último comentário publicado';
+$lang['module_desc_logon'] = 'Mostra loguin bloqueado';
+$lang['module_desc_mostvisited'] = 'Mostra x fotos mais visitadas na galeria ou numa categoria';
+$lang['module_desc_personal'] = 'Mostra bloqueados pessoais (como um editor)';
+?>
