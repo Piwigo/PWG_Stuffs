b35a3962e9c26784b5c4669f2dfd33e41dd56f7c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-11 16:55:06 +0000

    [pwg_stuffs] Add et_EE, thanks to : kersta
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@20071 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,99 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['module_desc_mostvisited'] = 'Näita x enim külastatud pilti galeriist või kategooriast';
+$lang['module_desc_citation'] = 'Näita juhuslikku tsitaati';
+$lang['module_desc_bestrated'] = 'Näita x enim hinnatud pilti galeriist või kategooriast';
+$lang['lastcoms_show_buttons'] = 'Näita administreerimis nuppe';
+$lang['module_name_citation'] = 'Juhuslik tsitaat';
+$lang['module_name_featuredphotos'] = 'Valitud pildid';
+$lang['stuffs_group_perm'] = 'gruppide põhjal';
+$lang['stuffs_level_perm'] = 'privaatsustasandi põhjal';
+$lang['stuffs_show_on_root'] = 'Kategooriate puu lehel';
+$lang['stuffs_type'] = 'Ploki tüüp';
+$lang['stuffs_unselect_all'] = 'Tühista kõik';
+$lang['stuffs_upgrade_message'] = 'PWG Stuffs plugin on ümbernimetatud. Uus nimetus "Blocks"';
+$lang['stuffs_user_perm'] = 'kasutajatüübi järgi';
+$lang['stuffs_width'] = 'Laius';
+$lang['stuffs_position'] = 'Positsioon';
+$lang['stuffs_public_module'] = '(hoia tühi avaliku ploki jaoks)';
+$lang['stuffs_select_all'] = 'Vali kõik';
+$lang['stuffs_show_module_on'] = 'Näita plokki:';
+$lang['stuffs_show_on_cats'] = 'Kõigil kategooria lehtedel';
+$lang['stuffs_show_on_home'] = 'Esilehel';
+$lang['stuffs_show_on_picture'] = 'Piltide lehel';
+$lang['stuffs_show_title'] = 'Näita ploki pealkirja';
+$lang['stuffs_tags_cloud'] = 'Märksõnade pilv';
+$lang['stuffs_mv_category_display'] = 'Näita kategooriate lehel';
+$lang['stuffs_mv_hold_ctrl'] = 'hoia all CTRL';
+$lang['stuffs_mv_nb_images'] = 'Maksimaalne näidatavate piltide arv';
+$lang['stuffs_mv_selected_cats'] = 'Ainult valitud kategooriad';
+$lang['stuffs_mv_w_thumb'] = 'Ainult pisipiltidega kategooriad';
+$lang['stuffs_mv_wo_thumb'] = 'Ainult pisipiltideta kategooriad';
+$lang['stuffs_no_name'] = 'Sa pead määrama ploki nime';
+$lang['stuffs_no_options'] = 'Ei ole valikuid selle ploki jaoks';
+$lang['stuffs_only_for_admin'] = '(ainult administreerimis paneelile)';
+$lang['stuffs_order_saved'] = 'Tellimus õnnestunult salvestatud';
+$lang['stuffs_perm'] = 'Aktiveeri autoriseerimine';
+$lang['stuffs_delete'] = 'Kustuta plokk';
+$lang['stuffs_desc'] = 'Kirjeldus';
+$lang['stuffs_edit'] = 'Modifitseeri plokki';
+$lang['stuffs_edit_mod'] = 'Toimeta plokki';
+$lang['stuffs_error_no_mod_selected'] = 'Sa ei ole ühtegi plokki valinud';
+$lang['stuffs_main_block'] = 'Põhiplokk';
+$lang['stuffs_mods_management'] = 'Plokkide korraldamine';
+$lang['stuffs_module_config'] = 'Ploki konfigureerimine';
+$lang['stuffs_module_options'] = 'Ploki valikud';
+$lang['stuffs_module_title'] = 'Ploki nimetus ';
+$lang['stuffs_mv_all_cats'] = 'Kõik kategooriad';
+$lang['stuffs_mv_cat_selection'] = 'Kategooriate valik';
+$lang['module_name_lastcoms'] = 'Viimased kommentaarid';
+$lang['module_name_logon'] = 'Logi sisse ';
+$lang['module_name_mostvisited'] = 'Enim külastatud';
+$lang['module_name_personal'] = 'Personaalne plokk';
+$lang['module_name_random'] = 'Juhuslikud pildid';
+$lang['module_name_recent'] = 'Viimatised pildid';
+$lang['module_name_tags'] = 'Märksõnad';
+$lang['personal_content'] = 'sisu';
+$lang['stuffs_action'] = 'Tegevused';
+$lang['stuffs_add_mod'] = 'Lisa plokk';
+$lang['stuffs_add_module'] = 'Lisa uus plokk';
+$lang['stuffs_authorized_group'] = 'Autoriseeritud grupid';
+$lang['stuffs_authorized_users'] = 'autoriseeritud kasutajad';
+$lang['stuffs_confirm_delete'] = 'Kas tahad kindlasti kustutada ploki %s?';
+$lang['module_desc_random'] = 'Näita x juhuslikku pilti galeriist või kategooriast';
+$lang['module_desc_recent'] = 'Näita x hiljutist pilti galeriist või kategooriast';
+$lang['module_desc_tags'] = 'Näita galerii märksõnu';
+$lang['module_name_bestrated'] = 'Kõrgeimalt hinnatud';
+$lang['Hide on home page'] = 'Peida esilehel';
+$lang['lastcoms_config'] = 'Konfiguratsioon';
+$lang['lastcoms_dim'] = 'Pisipildi suurus';
+$lang['lastcoms_height'] = 'Maksimaalne kõrgus';
+$lang['lastcoms_nb_comments'] = 'Näidatavate kommentaaride arv';
+$lang['lastcoms_no_nb_comments'] = 'Sa pead määrama kommentaaride arvu.';
+$lang['lastcoms_width'] = 'Maksimaalne kõrgus';
+$lang['module_Citation_code'] = 'Seotud kood';
+$lang['module_Citation_lang'] = 'Keel';
+$lang['module_desc_featuredphotos'] = 'Näita veebiomaniku lemmikfotosid';
+$lang['module_desc_lastcoms'] = 'Näita viimati postitatud kommentaare';
+$lang['module_desc_logon'] = 'Näita sisselogimisakent';
+?>
