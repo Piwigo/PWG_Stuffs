c6321426a2220a173d856a4a2525b5a5d7b99979
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-03 18:31:49 +0000

    [pwg_stuffs] Add bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@21147 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,110 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['stuffs_piclens_need_upgrade'] = 'PICLENS ПЛЪГИНА ТРЯБВА ДА СЕ ОБНОВИ. PWG Stuffs не е съвместим с тази версия на Piclens.';
+$lang['stuffs_parent_plugin_is_missing'] = 'ГЛАВНИЯ ПЛЪГИН НЕ Е АКТИВИРАН. Трябва да премахнете тази блок или повторно активиране родителския плъгин.';
+$lang['stuffs_tags_cumulus'] = 'Използване на режим (плъгин <a href=http://piwigo.org/ext/extension_view.php?eid=263 target="_blank">Cumulus Tags Cloud</a> трябва да е инсталиран)';
+$lang['module_desc_recent'] = 'Покажи х последни снимки в галерия или в категория (ако има)';
+$lang['module_desc_random'] = 'Покажи х случайни снимки в галерия или в категория';
+$lang['stuffs_upgrade_message'] = 'PWG Stuffs плъгина е преименуван. Вече се нарича "Blocks" (части).';
+$lang['stuffs_tags_cloud'] = 'Етикети (тагове)';
+$lang['stuffs_perm'] = 'Активиране управлението но разрешения';
+$lang['stuffs_confirm_delete'] = 'Потвърдете, че искате да изтриете част %s?';
+$lang['module_desc_personal'] = 'Покажи персовална част (като редактор)';
+$lang['module_desc_mostvisited'] = 'Покажи х най-посетени снимки в галерия или в категория';
+$lang['module_desc_citation'] = 'Показване на случайни цитати';
+$lang['module_desc_featuredphotos'] = 'Покажи избрани снимки от уебмастър';
+$lang['module_name_citation'] = 'Случаен цитат';
+$lang['stuffs_public_module'] = '(Запази празна в публичната част)';
+$lang['stuffs_only_for_admin'] = '(само за администраторския панел)';
+$lang['stuffs_no_options'] = 'Няма опции за тази част';
+$lang['module_desc_bestrated'] = 'Покажи x най-добри снимски в галерията или в категори';
+$lang['module_Citation_code'] = 'Свързан код';
+$lang['stuffs_no_name'] = 'Трябва да въведете име на част';
+$lang['stuffs_mv_wo_thumb'] = 'Само категории без миниатюри';
+$lang['stuffs_mv_w_thumb'] = 'Само категории с миниатюри';
+$lang['stuffs_error_no_mod_selected'] = 'Не сте избрали никоя част.';
+$lang['stuffs_order_saved'] = 'Избора е успешно записан';
+$lang['stuffs_mv_selected_cats'] = 'Само избраните категории';
+$lang['module_desc_tags'] = 'Покажи таговета на галерията';
+$lang['lastcoms_show_buttons'] = 'Покажи Админ бутоните';
+$lang['lastcoms_taille_cadres'] = 'Височина рамка';
+$lang['stuffs_show_on_cats'] = 'Във всички страници категории';
+$lang['stuffs_show_module_on'] = 'Покажи част:';
+$lang['stuffs_position'] = 'Позиция';
+$lang['stuffs_select_all'] = 'Избери всички';
+$lang['stuffs_unselect_all'] = 'Откажи избраните';
+$lang['stuffs_type'] = 'Тип част';
+$lang['stuffs_tags_letters'] = 'Групи до букви';
+$lang['stuffs_tags_default_display'] = 'Покажи тагове';
+$lang['stuffs_show_title'] = 'Покажи име на част';
+$lang['stuffs_show_on_root'] = 'На главната страница категории';
+$lang['stuffs_show_on_home'] = 'На основна страница';
+$lang['stuffs_show_on_picture'] = 'На страница снимки';
+$lang['stuffs_user_perm'] = 'по вид потребител';
+$lang['stuffs_width'] = 'Ширина';
+$lang['stuffs_visual_config'] = 'Екранна конфигурация';
+$lang['stuffs_mv_nb_images'] = 'Максимален брой снимки за показване';
+$lang['stuffs_module_options'] = 'Опции на част';
+$lang['stuffs_module_title'] = 'Име на част';
+$lang['stuffs_mv_hold_ctrl'] = 'Задръж CTRL клавиша';
+$lang['stuffs_mv_category_display'] = 'Покажи в страницата с категории';
+$lang['stuffs_mv_cat_selection'] = 'Избор на категории';
+$lang['stuffs_mv_all_cats'] = 'Всички категории';
+$lang['stuffs_module_config'] = 'Конфигурация на част';
+$lang['stuffs_mods_management'] = 'Управление на част';
+$lang['stuffs_main_block'] = 'Главна част';
+$lang['stuffs_authorized_group'] = 'Групи с разрешение';
+$lang['stuffs_authorized_users'] = 'Потребители с разрешение';
+$lang['stuffs_action'] = 'Действия';
+$lang['stuffs_level_perm'] = 'по ниво на достъп';
+$lang['stuffs_id_ligne'] = 'ID';
+$lang['stuffs_group_perm'] = 'по групи';
+$lang['stuffs_edit_mod'] = 'Редакция на част';
+$lang['stuffs_edit'] = 'Промяна на част';
+$lang['stuffs_desc'] = 'Описание';
+$lang['stuffs_delete'] = 'Изтриване на част';
+$lang['stuffs_add_mod'] = 'Добавяне на част';
+$lang['stuffs_add_module'] = 'Добави нова част';
+$lang['personal_content'] = 'коментар';
+$lang['module_name_tags'] = 'Тагове';
+$lang['module_name_lastcoms'] = 'Последни коментари';
+$lang['module_name_featuredphotos'] = 'Представени снимки';
+$lang['module_name_bestrated'] = 'Най-добра оценка';
+$lang['module_name_recent'] = 'Последни снимки';
+$lang['module_name_random'] = 'Случайна снимка';
+$lang['module_name_personal'] = 'Персонален раздел';
+$lang['module_name_mostvisited'] = 'Най-посетени';
+$lang['module_name_logon'] = 'Влизане';
+$lang['lastcoms_no_nb_comments'] = 'Трябва да зададете брой коментари';
+$lang['lastcoms_nb_comments'] = 'Брой на показвани коментари';
+$lang['lastcoms_nb_cadres'] = 'Брой на рамки на ред';
+$lang['lastcoms_height'] = 'Максимална височина';
+$lang['Hide on home page'] = 'Скриване от начална страница';
+$lang['lastcoms_dim'] = 'Размер миниатюри';
+$lang['lastcoms_config'] = 'Конфигурация';
+$lang['module_desc_logon'] = 'Покажи меню за логване';
+$lang['module_desc_lastcoms'] = 'Покажи последно публикуваните коментари';
+$lang['module_Citation_lang'] = 'Език';
+$lang['logon_remove_menubar_block'] = 'Премахни "Quick connect" от менюто';
+$lang['lastcoms_width'] = 'Максимална ширина';
+?>
