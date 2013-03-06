47e1e837c52e2faa3a5de3528c65c31103643637
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-06 16:07:47 +0000

    [pwg_stuffs] Add fa_IR, thanks to : PicaKing
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@21267 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['module_name_featuredphotos'] = 'عکس های ویژه';
+$lang['module_name_lastcoms'] = 'آخرین نظرات';
+$lang['Hide on home page'] = 'مخفی کردن در صفحه خانگی';
+$lang['lastcoms_config'] = 'پیکره بندی';
+$lang['lastcoms_dim'] = 'اندازه بندانگشتی';
+$lang['lastcoms_height'] = 'حداکثر ارتفاع';
+$lang['lastcoms_width'] = 'حداکثر عرض';
+$lang['module_Citation_code'] = 'کد مشابه';
+$lang['module_Citation_lang'] = 'زبان';
+?>
