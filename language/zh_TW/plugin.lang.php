aafafd75ed30eaac0e4e4ef01b9efa160c31ad18
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-25 21:23:37 +0000

    [pwg_stuffs] Add zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@21840 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['module_desc_citation'] = '顯示隨機引用';
+$lang['module_desc_featuredphotos'] = '顯示我的最愛照片的網站管理員';
+$lang['module_desc_lastcoms'] = '顯示上次發表評論';
+$lang['module_desc_logon'] = '顯示登入區塊';
+$lang['module_desc_mostvisited'] = '顯示X個訪問次數最多圖片庫中的的類別';
+$lang['module_desc_personal'] = '顯示個人區塊(像編輯器一樣)。';
+$lang['lastcoms_show_buttons'] = '顯示管理員按鈕';
+$lang['lastcoms_taille_cadres'] = '框架高度';
+$lang['lastcoms_width'] = '最大寬度';
+$lang['logon_remove_menubar_block'] = '從選單欄中刪除 "Quick connect" 區塊';
+$lang['module_Citation_code'] = '相關的程式碼';
+$lang['module_Citation_lang'] = '語言';
+$lang['module_desc_bestrated'] = '顯示X最受好評的照片在畫廊或類別';
+$lang['Hide on home page'] = '隱藏主頁';
+$lang['lastcoms_config'] = '設定';
+$lang['lastcoms_dim'] = '縮略圖尺寸';
+$lang['lastcoms_height'] = '最高高度';
+$lang['lastcoms_nb_cadres'] = '每行框架的數量';
+$lang['lastcoms_nb_comments'] = '要顯示的評論的的數量';
+$lang['lastcoms_no_nb_comments'] = '您必須指定數量的評論。';
+?>
