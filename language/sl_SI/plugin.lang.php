594b5b6c0a2aa173a787b4e03c013dace3d98687
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-11 14:55:17 +0000

    [pwg_stuffs] Add sl_SI, thanks to : MarkoAm
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@22609 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,110 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['stuffs_tags_letters'] = 'Grupiraj po črkah';
+$lang['stuffs_type'] = 'Vrsta sklopa';
+$lang['stuffs_unselect_all'] = 'Odznači vse';
+$lang['stuffs_upgrade_message'] = 'Vtičnik PWG Osebje je preimenovan. Sedaj se imenuje "Sklopi"';
+$lang['stuffs_user_perm'] = 'po vrsti uporabnika';
+$lang['stuffs_visual_config'] = 'Prikaži nastavitev';
+$lang['stuffs_width'] = 'Širina';
+$lang['stuffs_mv_wo_thumb'] = 'Samo kategorije brez sličic';
+$lang['stuffs_no_name'] = 'Določiti morate ime sklopa';
+$lang['stuffs_no_options'] = 'Ni dodatnih možnosti za ta sklop';
+$lang['stuffs_only_for_admin'] = '(samo za administratorsko ploščo)';
+$lang['stuffs_order_saved'] = 'Razpored uspešno shranjen.';
+$lang['stuffs_parent_plugin_is_missing'] = 'NADREJENI VTIČNIK NI AKTIVEN. Ta sklop odstranite ali pa aktivirajte nadrejeni vtičnik.';
+$lang['stuffs_perm'] = 'Vključite upravljanje avtorizacij:';
+$lang['stuffs_piclens_need_upgrade'] = 'PICLENS VTIČNIK MORATE POSODOBITI. "PWG Osebje" ni skladen s to verzijo Piclens.';
+$lang['stuffs_position'] = 'Pozicija';
+$lang['stuffs_public_module'] = '(pusti prazno za javni sklop)';
+$lang['stuffs_select_all'] = 'Izberi vse';
+$lang['stuffs_show_module_on'] = 'Prikaži sklop:';
+$lang['stuffs_show_on_cats'] = 'Na vseh straneh kategorij';
+$lang['stuffs_show_on_home'] = 'Na glavni strani';
+$lang['stuffs_show_on_picture'] = 'Na strani slik';
+$lang['stuffs_show_on_root'] = 'Na strani kategorij';
+$lang['stuffs_show_title'] = 'Prikaži ime sklopa';
+$lang['stuffs_tags_cloud'] = 'Oblaček oznak';
+$lang['stuffs_tags_cumulus'] = 'Uporabi kopasti način (vtičnik <a href=http://piwigo.org/ext/extension_view.php?eid=263 target="_blank">Kopasti oblaček oznak</a> mora biti nameščen)';
+$lang['stuffs_tags_default_display'] = 'Prikaži oznake';
+$lang['stuffs_delete'] = 'Izbriši sklop';
+$lang['stuffs_desc'] = 'Opis';
+$lang['stuffs_edit'] = 'Spremeni sklop';
+$lang['stuffs_edit_mod'] = 'Uredi sklop';
+$lang['stuffs_error_no_mod_selected'] = 'Niste izbrali nobenega sklopa';
+$lang['stuffs_group_perm'] = 'po skupinah';
+$lang['stuffs_id_ligne'] = 'ID vrste';
+$lang['stuffs_level_perm'] = 'po nivoju zasebnosti';
+$lang['stuffs_main_block'] = 'Glavni sklop';
+$lang['stuffs_mods_management'] = 'Urejanje sklopov';
+$lang['stuffs_module_config'] = 'Konfiguriranje sklopov';
+$lang['stuffs_module_options'] = 'Možnosti sklopov';
+$lang['stuffs_module_title'] = 'Ime sklopa';
+$lang['stuffs_mv_all_cats'] = 'Vse kategorije';
+$lang['stuffs_mv_cat_selection'] = 'Izbira kategorij';
+$lang['stuffs_mv_category_display'] = 'Prikaži na straneh kategorij';
+$lang['stuffs_mv_hold_ctrl'] = 'držite tipko CTRL';
+$lang['stuffs_mv_nb_images'] = 'Maksimalno število prikazanih slik';
+$lang['stuffs_mv_selected_cats'] = 'Samo izbrane kategorije';
+$lang['stuffs_mv_w_thumb'] = 'Samo kategorije s sličicami';
+$lang['module_desc_recent'] = 'Prikaži x nedavnih slik v galeriji ali kategoriji (če obstajajo)';
+$lang['module_desc_tags'] = 'Prikaži oznake galerije';
+$lang['module_name_bestrated'] = 'Najbolje ocenjene';
+$lang['module_name_citation'] = 'Naključni citat';
+$lang['module_name_featuredphotos'] = 'Prikazne slike';
+$lang['module_name_lastcoms'] = 'Zadnji komentarji';
+$lang['module_name_logon'] = 'Prijava';
+$lang['module_name_mostvisited'] = 'Najbolj gledane';
+$lang['module_name_personal'] = 'Osebni sklop';
+$lang['module_name_random'] = 'Naključne slike';
+$lang['module_name_recent'] = 'Nedavne slike';
+$lang['module_name_tags'] = 'Oznake';
+$lang['personal_content'] = 'vsebina';
+$lang['stuffs_action'] = 'Dejanja';
+$lang['stuffs_add_mod'] = 'Dodaj sklop';
+$lang['stuffs_add_module'] = 'Dodaj nov sklop';
+$lang['stuffs_authorized_group'] = 'Pooblaščene skupine';
+$lang['stuffs_authorized_users'] = 'Pooblaščeni uporabniki';
+$lang['stuffs_confirm_delete'] = 'Ste prepričani da želite odstraniti sklop %s?';
+$lang['module_desc_personal'] = 'Prikaži osebni sklop (kot urejevalnik)';
+$lang['module_desc_random'] = 'Prikaži x naključnih slik v galeriji ali kategoriji';
+$lang['Hide on home page'] = 'Skrij na domači strani';
+$lang['lastcoms_config'] = 'Nastavitve';
+$lang['lastcoms_dim'] = 'Velikost sličic';
+$lang['lastcoms_height'] = 'Maksimalna višina';
+$lang['lastcoms_nb_cadres'] = 'Število okvirjev v vrsti';
+$lang['lastcoms_nb_comments'] = 'Število prikazanih komentarjev';
+$lang['lastcoms_no_nb_comments'] = 'Določiti morate število prikazanih komentarjev.';
+$lang['lastcoms_show_buttons'] = 'Prikaži admin gumbe';
+$lang['lastcoms_taille_cadres'] = 'Višina okvirja';
+$lang['lastcoms_width'] = 'Maksimalna širina';
+$lang['logon_remove_menubar_block'] = 'Odstrani  sklop "Hitre povezave" iz menijske vrstice';
+$lang['module_Citation_code'] = 'Povezana koda';
+$lang['module_Citation_lang'] = 'Jezik';
+$lang['module_desc_bestrated'] = 'Prikaži x najbolj ocenjenih slik v galeriji ali kategoriji';
+$lang['module_desc_citation'] = 'Prikaži naključni citat';
+$lang['module_desc_featuredphotos'] = 'Prikaži priljubljene slike skrbnika';
+$lang['module_desc_lastcoms'] = 'Prikaži zadnje objavljene komentarje';
+$lang['module_desc_logon'] = 'Prikaži prijavni sklop';
+$lang['module_desc_mostvisited'] = 'Prikaži x največkrat ogledanih slik v galeriji ali kategoriji';
+?>
