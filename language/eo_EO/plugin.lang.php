8d92e58cd90171f443c5932c7ae06b5120e9e066
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-11-26 05:40:30 +0000

    [pwg_stuffs] Update eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@25713 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['module_desc_featuredphotos'] = 'Vidigi la preferatajn fotojn de la retestro';
+$lang['module_desc_lastcoms'] = 'Vidigi la freŝe redaktitajn komentojn';
+$lang['module_name_bestrated'] = 'Preferata';
+$lang['module_name_mostvisited'] = 'Plej vizitita';
+$lang['module_name_tags'] = 'Etikedoj';
+$lang['stuffs_action'] = 'Agoj';
+$lang['module_name_citation'] = 'Hazarda citaĵo';
+$lang['module_name_lastcoms'] = 'Lastaj komentoj';
+$lang['module_name_random'] = 'Hazardaj bildoj';
+$lang['module_name_recent'] = 'Freŝaj bildoj';
+$lang['Hide on home page'] = 'Kaŝi sur ĉefpaĝo';
+$lang['lastcoms_config'] = 'Agordo';
+$lang['lastcoms_dim'] = 'Miniatura grandeco';
+$lang['lastcoms_height'] = 'Maksimuma alteco';
+$lang['lastcoms_nb_cadres'] = 'Nombroj da kadroj sur ĉiu linio';
+$lang['lastcoms_nb_comments'] = 'Nombro da komentoj montrendaj';
+$lang['lastcoms_no_nb_comments'] = 'Vi devas precizi la nombrojn da komentoj.';
+$lang['lastcoms_show_buttons'] = 'Montri la agordajn ŝaltilojn';
+$lang['lastcoms_taille_cadres'] = 'Kadra alteco';
+$lang['lastcoms_width'] = 'Maksimuma larĝeco';
+$lang['module_Citation_lang'] = 'Lingvo';
+$lang['module_desc_citation'] = 'Vidigi hazardan citaĵon';
+?>
