15dfa6efa961381578a256e17962cd8832135976
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-25 06:41:41 +0000

    [pwg_stuffs] Update ro_RO, thanks to : cheilenereinfo
    git-svn-id: http://piwigo.org/svn/extensions/PWG_Stuffs/trunk@26963 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ro_RO/plugin.lang.php
@@ -0,0 +1,80 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['stuffs_piclens_need_upgrade'] = 'PLUGIN-UL PICLENS TREBUIE ACTUALIZAT. Componentele PWG nu sunt compatibile cu versiunea aceasta a pluginului Piclens.';
+$lang['stuffs_show_module_on'] = 'Arata bloc:';
+$lang['stuffs_select_all'] = 'Selecteaza tot';
+$lang['stuffs_position'] = 'Pozitie';
+$lang['stuffs_mv_nb_images'] = 'Numarul maxim de fotografii de afisat';
+$lang['stuffs_order_saved'] = 'Ordinea salvata cu succes';
+$lang['stuffs_only_for_admin'] = '(doar pentru panoul de administrare)';
+$lang['stuffs_no_options'] = 'Nici o optiune pentru acest bloc';
+$lang['stuffs_no_name'] = 'Trebuie sa specifici numele blocului';
+$lang['stuffs_mv_wo_thumb'] = 'Doar categoriile fara miniaturi';
+$lang['stuffs_mv_w_thumb'] = 'Doar categoriile cu miniaturi';
+$lang['stuffs_mv_selected_cats'] = 'Doar categoriile selectate';
+$lang['stuffs_mv_hold_ctrl'] = 'apasa CTRL';
+$lang['stuffs_mv_all_cats'] = 'Toate categoriile';
+$lang['stuffs_module_title'] = 'Titlul blocului';
+$lang['stuffs_module_options'] = 'Optiunile blocului';
+$lang['stuffs_module_config'] = 'Configurarea blocului';
+$lang['stuffs_main_block'] = 'Blocul principal';
+$lang['stuffs_add_module'] = 'Adauga un bloc nou';
+$lang['stuffs_add_mod'] = 'Adauga bloc';
+$lang['stuffs_group_perm'] = 'dupa grupuri';
+$lang['stuffs_error_no_mod_selected'] = 'Nu ai selectat nici un bloc';
+$lang['stuffs_edit_mod'] = 'Editare bloc';
+$lang['stuffs_edit'] = 'Modifica bloc';
+$lang['stuffs_desc'] = 'Descriere';
+$lang['stuffs_delete'] = 'Sterge bloc';
+$lang['stuffs_confirm_delete'] = 'Esti sigur ca vrei sa stergi blocul %s?';
+$lang['stuffs_authorized_users'] = 'Utilizatori autorizati';
+$lang['stuffs_authorized_group'] = 'Grupuri autorizate';
+$lang['stuffs_action'] = 'Actiuni';
+$lang['personal_content'] = 'continut';
+$lang['module_name_tags'] = 'Etichete';
+$lang['module_name_recent'] = 'Fotografii recente';
+$lang['module_name_random'] = 'Fotografii aleatoare';
+$lang['module_name_mostvisited'] = 'Cele mai vizualizate';
+$lang['module_name_logon'] = 'Autentificare';
+$lang['module_name_lastcoms'] = 'Ultimele comentarii';
+$lang['module_name_citation'] = 'Citat aleatoriu';
+$lang['module_name_bestrated'] = 'Ce mai bine evaluate';
+$lang['module_desc_tags'] = 'Afiseaza etichetele galeriei';
+$lang['module_desc_recent'] = 'Afiseaza x poze recente din galerie sau dintr-o categorie (daca exista)';
+$lang['module_desc_random'] = 'Afiseaza x poze aleatoare din galerie sau dintr-o categorie';
+$lang['module_desc_mostvisited'] = 'Afiseaza x cele mai vizualizate poze din galerie sau dintr-o categorie';
+$lang['module_desc_lastcoms'] = 'Afiseaza ultimele comentarii postate';
+$lang['module_desc_featuredphotos'] = 'Afiseaza pozele favorite ale administatorului';
+$lang['module_desc_citation'] = 'Afiseaza un citat aleatoriu';
+$lang['module_desc_bestrated'] = 'Afiseaza x cele mai bine evaluate poze din galerie sau dintr-o categorie';
+$lang['module_Citation_lang'] = 'Limba';
+$lang['lastcoms_width'] = 'Latimea maxima';
+$lang['lastcoms_taille_cadres'] = 'Inaltimea ramei';
+$lang['lastcoms_show_buttons'] = 'Arata butoanele administratorului';
+$lang['lastcoms_no_nb_comments'] = 'Trebuie sa specifici numarul de comentarii';
+$lang['lastcoms_nb_comments'] = 'Numarul de comentarii de afisat';
+$lang['lastcoms_height'] = 'Inaltimea maxima';
+$lang['lastcoms_dim'] = 'Dimensiunea miniaturilor';
+$lang['lastcoms_config'] = 'Configuratie';
+$lang['Hide on home page'] = 'Ascunde in pagina principala';
+?>
