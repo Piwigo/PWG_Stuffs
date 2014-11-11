b242c444425e4fb13ab5dff7703b1e4eef0338be
Piwigo-TranslationTeam <Piwigo-TranslationTeam@users.noreply.github.com>
2014-11-11 10:01:34 +0100

    [pwg_stuffs] Update nb_NO, thanks to paulen

--- language/nb_NO/plugin.lang.php
+++ language/nb_NO/plugin.lang.php
@@ -1,55 +1,55 @@
 <?php
 
-/*TODO*/$lang['stuffs_mods_management'] = 'Blocks management';
-/*TODO*/$lang['stuffs_add_mod'] = 'Add a block';
-/*TODO*/$lang['stuffs_edit_mod'] = 'Edit block';
+$lang['stuffs_mods_management'] = 'Blokk styring';
+$lang['stuffs_add_mod'] = 'Legg til en blokk';
+$lang['stuffs_edit_mod'] = 'Edit blokk';
 
-/*TODO*/$lang['stuffs_module_title'] = 'Block title';
-/*TODO*/$lang['stuffs_desc'] = 'Description';
-/*TODO*/$lang['stuffs_only_for_admin'] = '(only for administration pannel)';
-/*TODO*/$lang['stuffs_type'] = 'Block type';
-/*TODO*/$lang['stuffs_position'] = 'Position';
-/*TODO*/$lang['stuffs_id_ligne'] = 'ID line';
-/*TODO*/$lang['stuffs_width'] = 'Width';
-/*TODO*/$lang['stuffs_action'] = 'Actions';
-/*TODO*/$lang['stuffs_edit'] = 'Modify block';
-/*TODO*/$lang['stuffs_delete'] = 'Delete block';
-/*TODO*/$lang['stuffs_confirm_delete'] = 'Are you sure you want to delete block %s?';
-/*TODO*/$lang['stuffs_main_block'] = 'Main block';
-/*TODO*/$lang['Hide on home page'] = 'Hide on home page';
+$lang['stuffs_module_title'] = 'Blokk tittel';
+$lang['stuffs_desc'] = 'Beskrivelse';
+$lang['stuffs_only_for_admin'] = '(kun for administrasjons panel)';
+$lang['stuffs_type'] = 'Blokk type';
+$lang['stuffs_position'] = 'Posisjon';
+$lang['stuffs_id_ligne'] = 'ID linje';
+$lang['stuffs_width'] = 'Bredde';
+$lang['stuffs_action'] = 'Handling';
+$lang['stuffs_edit'] = 'Endre blokk';
+$lang['stuffs_delete'] = 'Slett blokk';
+$lang['stuffs_confirm_delete'] = 'Er du sikker på at du ønsker å slette blokken %s?';
+$lang['stuffs_main_block'] = 'Hoved blokk';
+$lang['Hide on home page'] = 'Skjul på hjemmesiden';
 
-/*TODO*/$lang['stuffs_add_module'] = 'Add new block';
-/*TODO*/$lang['stuffs_module_config'] = 'Block configuration';
-/*TODO*/$lang['stuffs_visual_config'] = 'Display configuration';
-/*TODO*/$lang['stuffs_module_options'] = 'Block options';
-/*TODO*/$lang['stuffs_authorized_group'] = 'Authorized groups';
-/*TODO*/$lang['stuffs_authorized_users'] = 'Authorized users';
-/*TODO*/$lang['stuffs_public_module'] = '(Keep empty for public block)';
-/*TODO*/$lang['stuffs_show_title'] = 'Show block title';
-/*TODO*/$lang['stuffs_show_module_on'] = 'Show block :';
-/*TODO*/$lang['stuffs_show_on_home'] = 'On main page';
-/*TODO*/$lang['stuffs_show_on_root'] = 'On categories root page';
-/*TODO*/$lang['stuffs_show_on_cats'] = 'On all categories pages';
-/*TODO*/$lang['stuffs_show_on_picture'] = 'On picture pages';
-/*TODO*/$lang['stuffs_no_options'] = 'No options for this block';
+$lang['stuffs_add_module'] = 'Legg til en ny blokk';
+$lang['stuffs_module_config'] = 'Blokk innstilling';
+$lang['stuffs_visual_config'] = 'Vis innstillinger';
+$lang['stuffs_module_options'] = 'Blokk alternativer';
+$lang['stuffs_authorized_group'] = 'Autoriserte grupper';
+$lang['stuffs_authorized_users'] = 'Autoriserte brukere';
+$lang['stuffs_public_module'] = '(Hold tomt for offentlig blokk)';
+$lang['stuffs_show_title'] = 'Vis blokk tittel';
+$lang['stuffs_show_module_on'] = 'Vis blokk:';
+$lang['stuffs_show_on_home'] = 'På hoved siden';
+$lang['stuffs_show_on_root'] = 'På kategoriers rotside';
+$lang['stuffs_show_on_cats'] = 'På alle kategori sider';
+$lang['stuffs_show_on_picture'] = 'På bilde siden';
+$lang['stuffs_no_options'] = 'Ingen altenativer for denne blokken';
 
-/*TODO*/$lang['stuffs_perm'] = 'Activate authorization management:';
-/*TODO*/$lang['stuffs_group_perm'] = 'by groups';
-/*TODO*/$lang['stuffs_user_perm'] = 'by user type';
-/*TODO*/$lang['stuffs_level_perm'] = 'by privacy level';
+$lang['stuffs_perm'] = 'Aktiver autorisasjon ledelse:';
+$lang['stuffs_group_perm'] = 'av grupper';
+$lang['stuffs_user_perm'] = 'etter brukertype';
+$lang['stuffs_level_perm'] = 'av personvernnivå';
 
-/*TODO*/$lang['stuffs_select_all'] = 'Select all';
-/*TODO*/$lang['stuffs_unselect_all'] = 'Unselect all';
+$lang['stuffs_select_all'] = 'Velg alle';
+$lang['stuffs_unselect_all'] = 'Opphev alle';
 
 // Infos messages
-/*TODO*/$lang['stuffs_order_saved'] = 'Order successfully saved.';
-/*TODO*/$lang['stuffs_upgrade_message'] = 'PWG Stuffs plugin as been renamed. It is now called "Blocks".';
+$lang['stuffs_order_saved'] = 'Bestilling vellykket lagret.';
+$lang['stuffs_upgrade_message'] = 'Programtillegget PWG Stuffs har fått nytt navn. Det blir nå kalt "blokker".';
 
 // Errors messages
-/*TODO*/$lang['stuffs_error_no_mod_selected'] = 'You didn\'t select any block.';
-/*TODO*/$lang['stuffs_no_name'] = 'You must specify block\'s name';
-/*TODO*/$lang['stuffs_parent_plugin_is_missing'] = 'PARENT PLUGIN IS NOT ACTIVATED. You should remove this block or re-activate parent plugin.';
-/*TODO*/$lang['stuffs_piclens_need_upgrade'] = 'PICLENS PLUGIN MUST BE UPDATED. PWG Stuffs is not compatible with this version of Piclens.';
+$lang['stuffs_error_no_mod_selected'] = 'Du valgte ingen blokk.';
+$lang['stuffs_no_name'] = 'Du må spesifisere navnet på blokken';
+$lang['stuffs_parent_plugin_is_missing'] = 'PROGRAMTILLEGGET OVER ER IKKE AKTIVERT. Du bør fjerne denne blokken eller re-aktivere programtillegget over.';
+$lang['stuffs_piclens_need_upgrade'] = 'PROGRAMTILLEGGET PICLENS MÅ OPPDATERES. PWG Stuffs er ikke kompatibel med denne versjonen av Piclens.';
 
 //------------------------------------------------------------------------------
 //                            Modul LastComs
@@ -59,10 +59,10 @@ $lang['module_name_lastcoms'] = 'Siste kommentarer';
 $lang['module_desc_lastcoms'] = 'Vis siste postede kommentar';
 
 $lang['lastcoms_no_nb_comments'] = 'Du må spesifisere antall kommentarer.';
-$lang['lastcoms_config'] = 'Konfigurasjon';
+$lang['lastcoms_config'] = 'Innstilling';
 $lang['lastcoms_nb_comments'] = 'Antall kommentarer å vise';
 $lang['lastcoms_show_buttons'] = 'Vis admin knapper';
-$lang['lastcoms_dim'] = 'Miniatyr bilde størrelse';
+$lang['lastcoms_dim'] = 'Miniatyrbilde størrelse';
 $lang['lastcoms_width'] = 'Maks bredde';
 $lang['lastcoms_height'] = 'Maks høyde';
 $lang['lastcoms_nb_cadres'] = 'Antall rammer per linje';
@@ -99,7 +99,7 @@ $lang['module_desc_tags'] = 'Vis galleri tagger';
 $lang['stuffs_tags_default_display'] = 'Tagg visning';
 $lang['stuffs_tags_cloud'] = 'Tagg sky';
 $lang['stuffs_tags_letters'] = 'Gruper alfabetisk';
-$lang['stuffs_tags_cumulus'] = 'Bruk cumulus modus (plugin <a href=http://piwigo.org/ext/extension_view.php?eid=263 target="_blank">Cumulus Tagger Cloud</a> må være installert)';
+$lang['stuffs_tags_cumulus'] = 'Bruk globular modus (programtillegg <a href=http://piwigo.org/ext/extension_view.php?eid=263 target="_blank">Cumulus Tagger Cloud</a> må være installert)';
 
 
 //------------------------------------------------------------------------------
@@ -107,16 +107,16 @@ $lang['stuffs_tags_cumulus'] = 'Bruk cumulus modus (plugin <a href=http://piwigo
 //------------------------------------------------------------------------------
 
 $lang['module_name_mostvisited'] = 'Mest besøkte';
-$lang['module_desc_mostvisited'] = 'Vis x mest besøkte bilder i galleri eller album';
+$lang['module_desc_mostvisited'] = 'Vis x mest besøkte bilder i galleriet eller en kategori';
 
-$lang['stuffs_mv_nb_images'] = 'Maksimum antall bilderå vise';
-$lang['stuffs_mv_category_display'] = 'Vis album sider';
-$lang['stuffs_mv_all_cats'] = 'Alle album';
-$lang['stuffs_mv_w_thumb'] = 'Kun album med miniatyr bilder';
-$lang['stuffs_mv_wo_thumb'] = 'Kun album uten miniatyr bilder';
-$lang['stuffs_mv_selected_cats'] = 'Kun utvalgte album';
+$lang['stuffs_mv_nb_images'] = 'Maksimum antall bilder å vise';
+$lang['stuffs_mv_category_display'] = 'Vis på kategori sider';
+$lang['stuffs_mv_all_cats'] = 'Alle kategorier';
+$lang['stuffs_mv_w_thumb'] = 'Kun kategorier med miniatyrbilder';
+$lang['stuffs_mv_wo_thumb'] = 'Kun kategorier uten miniatyrbilder';
+$lang['stuffs_mv_selected_cats'] = 'Kun utvalgte kategorier';
 
-$lang['stuffs_mv_cat_selection'] = 'Album valg';
+$lang['stuffs_mv_cat_selection'] = 'Kategori valg';
 $lang['stuffs_mv_hold_ctrl'] = 'hold CTRL';
 
 //------------------------------------------------------------------------------
@@ -124,21 +124,21 @@ $lang['stuffs_mv_hold_ctrl'] = 'hold CTRL';
 //------------------------------------------------------------------------------
 
 $lang['module_name_bestrated'] = 'Best rangerte';
-$lang['module_desc_bestrated'] = 'Vis x best rangerte bilder i galleriet eller et album';
+$lang['module_desc_bestrated'] = 'Vis x best rangerte bilder i galleriet eller en kategori';
 
 //------------------------------------------------------------------------------
 //                            Modul Siste
 //------------------------------------------------------------------------------
 
 $lang['module_name_recent'] = 'Siste bilder';
-$lang['module_desc_recent'] = 'Vis x siste bilder i galleriet eller album (om det eksisterer)';
+$lang['module_desc_recent'] = 'Vis x siste bilder i galleriet eller en kategori (om det eksisterer)';
 
 //------------------------------------------------------------------------------
 //                            Modul Tilfeldig
 //------------------------------------------------------------------------------
 
 $lang['module_name_random'] = 'Tilfeldige bilder';
-$lang['module_desc_random'] = 'Vis x tilfeldige bilder i galleriet eller i album';
+$lang['module_desc_random'] = 'Vis x tilfeldige bilder i galleriet eller i en kategori';
 
 //------------------------------------------------------------------------------
 //                            Modul Sitater
@@ -156,4 +156,4 @@ $lang['module_Citation_code'] = 'Relatert kode';
 $lang['module_name_featuredphotos'] = 'Kommende bilder';
 $lang['module_desc_featuredphotos'] = 'Vis favoritt bildene til webmasteren';
 
-?>
+$lang['stuffs_mv_recent_period_only'] = 'Vis kun bilder fra siste periode';
