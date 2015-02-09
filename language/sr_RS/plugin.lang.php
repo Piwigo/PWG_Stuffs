34661b560c5dd8835939d25a98313db76487db51
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-09 06:42:13 +0100

    Update sr_RS, thanks to gpopac

---
+++ language/sr_RS/plugin.lang.php
@@ -0,0 +1,110 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['stuffs_type'] = 'Врста блока';
+$lang['stuffs_no_name'] = 'Морате да дате назив овом блоку';
+$lang['stuffs_only_for_admin'] = '(само за администратора блока)';
+$lang['stuffs_parent_plugin_is_missing'] = 'РОДИТЕЉСКИ ДОДАТАК НИЈЕ АКТИВАН. Уклоните овај блок или поново активирајте потребни додатак.';
+$lang['stuffs_public_module'] = '(Оставите празно ако желите да блок буде јаван)';
+$lang['stuffs_show_title'] = 'Прикажи наслов блока';
+$lang['stuffs_delete'] = 'Обриши блок';
+$lang['stuffs_edit'] = 'Измени блок';
+$lang['stuffs_edit_mod'] = 'Уреди блок';
+$lang['stuffs_error_no_mod_selected'] = 'Нисте изабрали ни један блок';
+$lang['stuffs_main_block'] = 'Главни блок';
+$lang['stuffs_mods_management'] = 'Уређивање блокова';
+$lang['stuffs_module_config'] = 'Подешавање блока';
+$lang['stuffs_module_options'] = 'Опције блока';
+$lang['stuffs_module_title'] = 'Наслов блока';
+$lang['stuffs_add_mod'] = 'Додај блок';
+$lang['stuffs_add_module'] = 'Додај нови блок';
+$lang['stuffs_confirm_delete'] = 'Да ли желите да обришете блок %s?';
+$lang['module_desc_personal'] = 'Приказује блок са личним подацима';
+$lang['stuffs_tags_default_display'] = 'Приказ ознака';
+$lang['stuffs_tags_letters'] = 'Групиши по знаковима';
+$lang['stuffs_unselect_all'] = 'Поништи сав избор';
+$lang['stuffs_upgrade_message'] = 'PWG додатак се сада зове „Блокови“.';
+$lang['stuffs_user_perm'] = 'по врсти корисника';
+$lang['stuffs_visual_config'] = 'Прикажи подешавања';
+$lang['stuffs_width'] = 'Ширина';
+$lang['stuffs_tags_cumulus'] = 'Користи облак са ознакама (потребан је додатак: <a href=http://piwigo.org/ext/extension_view.php?eid=263 target="_blank">Кумулусни облак са ознакама</a>)';
+$lang['stuffs_perm'] = 'Омогући управљање пријавама:';
+$lang['stuffs_piclens_need_upgrade'] = 'МОРАТЕ ДА НАДОГРАДИТЕ PICLENS ДОДАТАК. PWG не може да ради са овом верзијом Piclens додатка.';
+$lang['stuffs_position'] = 'Позиција';
+$lang['stuffs_select_all'] = 'Изабери све';
+$lang['stuffs_show_module_on'] = 'Прикажи блок:';
+$lang['stuffs_show_on_cats'] = 'На свим страницама категорија';
+$lang['stuffs_show_on_home'] = 'На главној страници';
+$lang['stuffs_show_on_picture'] = 'На страници са сликама';
+$lang['stuffs_show_on_root'] = 'На категоријама у кореној страници';
+$lang['stuffs_tags_cloud'] = 'Облак од ознака';
+$lang['stuffs_mv_w_thumb'] = 'Само категорије са умањеним сликама';
+$lang['stuffs_mv_wo_thumb'] = 'Само категорије без умањених слика';
+$lang['stuffs_no_options'] = 'Овај блок нема опција';
+$lang['stuffs_order_saved'] = 'Редослед је успешно сачуван.';
+$lang['stuffs_desc'] = 'Опис';
+$lang['stuffs_group_perm'] = 'по групама';
+$lang['stuffs_id_ligne'] = 'Линија бројчане одреднице';
+$lang['stuffs_level_perm'] = 'по нивоу приватности';
+$lang['stuffs_mv_all_cats'] = 'Све категорије';
+$lang['stuffs_mv_cat_selection'] = 'Избор категорија';
+$lang['stuffs_mv_category_display'] = 'Прикажи на страници са категоријама';
+$lang['stuffs_mv_hold_ctrl'] = 'држите CTRL';
+$lang['stuffs_mv_nb_images'] = 'Највећи број слика за приказ';
+$lang['stuffs_mv_recent_period_only'] = 'Прикажи само скорашње слике';
+$lang['stuffs_mv_selected_cats'] = 'Само изабране категорије';
+$lang['module_Citation_code'] = 'Додатни код';
+$lang['module_desc_bestrated'] = 'Приказује x најбоље оцењених слика у галерији или категорији';
+$lang['module_desc_citation'] = 'Приказује насумични цитат';
+$lang['module_desc_featuredphotos'] = 'Приказује омиљене слике администратора';
+$lang['module_desc_lastcoms'] = 'Приказује последње коментаре';
+$lang['module_desc_logon'] = 'Приказује поље за пријаву';
+$lang['module_desc_mostvisited'] = 'Приказује x најпосећенијих слика у галерији или категорији';
+$lang['module_desc_random'] = 'Приказује x насумичних слика у галерији или категорији';
+$lang['module_desc_recent'] = 'Приказује x скорашњих слика у галерији или категорији (уколико постоји)';
+$lang['module_desc_tags'] = 'Приказује ознаке из галерије';
+$lang['module_name_bestrated'] = 'Најбоље оцењене';
+$lang['module_name_citation'] = 'Насумичан цитат';
+$lang['module_name_featuredphotos'] = 'Истакнуте слике';
+$lang['module_name_lastcoms'] = 'Последњи коментари';
+$lang['module_name_logon'] = 'Пријава';
+$lang['module_name_mostvisited'] = 'Најпосећеније';
+$lang['module_name_personal'] = 'Лични подаци';
+$lang['module_name_random'] = 'Насумична слика';
+$lang['module_name_recent'] = 'Скорашње слике';
+$lang['module_name_tags'] = 'Ознаке';
+$lang['personal_content'] = 'садржај';
+$lang['stuffs_action'] = 'Радње';
+$lang['stuffs_authorized_group'] = 'Овлашћене групе';
+$lang['stuffs_authorized_users'] = 'Овлашћени корисници';
+$lang['Hide on home page'] = 'Сакриј на почетној страници';
+$lang['lastcoms_config'] = 'Подешавања';
+$lang['lastcoms_dim'] = 'Величина умањених сличица';
+$lang['lastcoms_height'] = 'Највећа висина';
+$lang['lastcoms_nb_cadres'] = 'Број сличица по линији';
+$lang['lastcoms_nb_comments'] = 'Број приказаних коментара';
+$lang['lastcoms_no_nb_comments'] = 'Морате одредити број коментара.';
+$lang['lastcoms_show_buttons'] = 'Прикажи администраторске дугмиће';
+$lang['lastcoms_taille_cadres'] = 'Висина сличице';
+$lang['lastcoms_width'] = 'Највећа ширина';
+$lang['logon_remove_menubar_block'] = 'Уклони „Брзо повезивање“ из траке са менијем';
+$lang['module_Citation_lang'] = 'Језик';
