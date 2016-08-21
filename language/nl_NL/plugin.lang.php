498230e34c480a1e448c01ab34aea0fbd715d37f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-08-21 06:45:33 +0200

    Update nl_NL, thanks to watertoren5044

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -1,12 +1,12 @@
 <?php
 
-$lang['stuffs_mods_management'] = 'Blok beheer';
+$lang['stuffs_mods_management'] = 'Beheer blokken ';
 $lang['stuffs_add_mod'] = 'Voeg blok toe';
 $lang['stuffs_edit_mod'] = 'Blok bewerken';
 
-$lang['stuffs_module_title'] = 'Blok titel';
+$lang['stuffs_module_title'] = 'Bloktitel';
 $lang['stuffs_desc'] = 'Omschrijving';
-$lang['stuffs_only_for_admin'] = '(alleen voor administratie paneel)';
+$lang['stuffs_only_for_admin'] = '(alleen voor beheer-paneel)';
 $lang['stuffs_type'] = 'Blok type';
 $lang['stuffs_position'] = 'Positie';
 $lang['stuffs_id_ligne'] = 'ID regel';
@@ -14,28 +14,28 @@ $lang['stuffs_width'] = 'Breedte';
 $lang['stuffs_action'] = 'Acties';
 $lang['stuffs_edit'] = 'Bewerk blok';
 $lang['stuffs_delete'] = 'Verwijder blok';
-$lang['stuffs_confirm_delete'] = 'Weet je zeker dat je blok wil %s verwijderen?';
-$lang['stuffs_main_block'] = 'Hoofd blok';
-$lang['Hide on home page'] = 'Verberg op start pagina';
+$lang['stuffs_confirm_delete'] = 'Weet je zeker dat je blok wilt %s verwijderen?';
+$lang['stuffs_main_block'] = 'Hoofdblok';
+$lang['Hide on home page'] = 'Verberg op startpagina';
 
 $lang['stuffs_add_module'] = 'Voeg nieuw blok toe';
-$lang['stuffs_module_config'] = 'Blok configuratie';
-$lang['stuffs_visual_config'] = 'Weergave configuratie';
-$lang['stuffs_module_options'] = 'Blok opties';
+$lang['stuffs_module_config'] = 'Configuratie blok ';
+$lang['stuffs_visual_config'] = 'Configuratie weergave ';
+$lang['stuffs_module_options'] = 'Blok-opties';
 $lang['stuffs_authorized_group'] = 'Geautoriseerde groepen';
 $lang['stuffs_authorized_users'] = 'Geautoriseerde gebruikers';
-$lang['stuffs_public_module'] = '(Laat leeg voor publiek blok)';
+$lang['stuffs_public_module'] = '(Laat leeg voor openbaar blok)';
 $lang['stuffs_show_title'] = 'Toon blok titel';
-$lang['stuffs_show_module_on'] = 'Toon blok :';
-$lang['stuffs_show_on_home'] = 'Op hoofd pagina';
-$lang['stuffs_show_on_root'] = 'Op hoofd categorie pagina';
-$lang['stuffs_show_on_cats'] = 'Op alle categorie pagina\'s';
-$lang['stuffs_show_on_picture'] = 'Op afbeeldingspagina\'s';
+$lang['stuffs_show_module_on'] = 'Toon blok:';
+$lang['stuffs_show_on_home'] = 'Op hoofdpagina';
+$lang['stuffs_show_on_root'] = 'Op hoofdcategorie-pagina';
+$lang['stuffs_show_on_cats'] = 'Op alle categorieÃ«n-pagina\'s';
+$lang['stuffs_show_on_picture'] = 'Op fotopagina\'s';
 $lang['stuffs_no_options'] = 'Geen opties voor dit blok';
 
 $lang['stuffs_perm'] = 'Activeer autorisatie beheer:';
 $lang['stuffs_group_perm'] = 'per groep';
-$lang['stuffs_user_perm'] = 'per gebruikers type';
+$lang['stuffs_user_perm'] = 'Gebruiker per soort';
 $lang['stuffs_level_perm'] = 'naar privacy niveau';
 
 $lang['stuffs_select_all'] = 'Selecteer alles';
@@ -43,14 +43,14 @@ $lang['stuffs_unselect_all'] = 'Deselecteer alles';
 
 //todo
 // Infos messages
-$lang['stuffs_order_saved'] = 'Opslaan opdracht geslaagd.';
+$lang['stuffs_order_saved'] = 'Opdracht bewaren geslaagd.';
 $lang['stuffs_upgrade_message'] = 'De naam van PWG Stuffs plug-in is gewijzigd. Vanaf nu heet het "Blocks".';
 
 // Errors messages
 $lang['stuffs_error_no_mod_selected'] = 'Er is geen blok geselecteerd.';
 $lang['stuffs_no_name'] = 'Er moet een blok-naam worden opgegeven.';
 $lang['stuffs_parent_plugin_is_missing'] = 'VEREISTE PLUG-IN IS NIET GEACTIVEERD. Verwijder dit blok, of heractiveer de plugin waar dit blok van afhankelijk is.';
-$lang['stuffs_piclens_need_upgrade'] = 'PICLENS PLUGIN MOET WORDEN GEÜPDATET. PWG Stuffs werkt niet met deze verzie van Piclens.';
+$lang['stuffs_piclens_need_upgrade'] = 'PICLENS PLUGIN MOET WORDEN GEÃœPDATET. PWG Stuffs werkt niet met deze versie van Piclens.';
 
 //------------------------------------------------------------------------------
 //              Module LastComs
@@ -63,7 +63,7 @@ $lang['lastcoms_no_nb_comments'] = 'Er moet een aantal worden opgegeven.';
 $lang['lastcoms_config'] = 'Configuratie';
 $lang['lastcoms_nb_comments'] = 'Aantal te tonen reacties';
 $lang['lastcoms_show_buttons'] = 'Toon beheerder knoppen';
-$lang['lastcoms_dim'] = 'Miniatuur afmeting';
+$lang['lastcoms_dim'] = 'Miniatuur-afmeting';
 $lang['lastcoms_width'] = 'Maximale breedte';
 $lang['lastcoms_height'] = 'Maximale hoogte';
 $lang['lastcoms_nb_cadres'] = 'Aantal frames per regel';
@@ -74,9 +74,9 @@ $lang['lastcoms_taille_cadres'] = 'Frame hoogte';
 //------------------------------------------------------------------------------
 
 $lang['module_name_logon'] = 'Log in';
-$lang['module_desc_logon'] = 'Toon inlog blok';
+$lang['module_desc_logon'] = 'Toon inlog-blok';
 
-$lang['logon_remove_menubar_block'] = 'Verwijder het "Snel inloggen" blok uit de menubalk';
+$lang['logon_remove_menubar_block'] = 'Verwijder het "Snel inloggen"-blok uit de menubalk';
 
 //------------------------------------------------------------------------------
 //              Module Personal
@@ -97,23 +97,23 @@ $lang['module_desc_tags'] = 'Toon album tags';
 $lang['stuffs_tags_default_display'] = 'Tags weergave';
 $lang['stuffs_tags_cloud'] = 'Tag wolk';
 $lang['stuffs_tags_letters'] = 'Groeperen op letter';
-$lang['stuffs_tags_cumulus'] = 'Gebruik cumulus modus (de plug-in <a href=http://piwigo.org/ext/extension_view.php?eid=263 target="_blank">Cumulus Tags Cloud</a> moet zijn geïnstalleerd)';
+$lang['stuffs_tags_cumulus'] = 'Gebruik cumulus modus (de plug-in <a href=http://piwigo.org/ext/extension_view.php?eid=263 target="_blank">Cumulus Tags Cloud</a> moet zijn geÃ¯nstalleerd)';
 
 //------------------------------------------------------------------------------
 //              Module Most Visited
 //------------------------------------------------------------------------------
 
 $lang['module_name_mostvisited'] = 'Vaak bezocht';
-$lang['module_desc_mostvisited'] = 'Toon de x meest bezochte afbeeldingen in het album of de categorie';
+$lang['module_desc_mostvisited'] = 'Toon de x meest bezochte foto\'s in het album of een categorie';
 
-$lang['stuffs_mv_nb_images'] = 'Maximum aantal weer te geven afbeeldingen';
-$lang['stuffs_mv_category_display'] = 'Toon op categorieën pagina\'s';
-$lang['stuffs_mv_all_cats'] = 'Alle categorieën';
-$lang['stuffs_mv_w_thumb'] = 'Alleen categorieën met miniatuur afbeeldingen';
-$lang['stuffs_mv_wo_thumb'] = 'Alleen categorieën zonder miniatuur afbeeldingen';
-$lang['stuffs_mv_selected_cats'] = 'Alleen geselecteerde categorieën';
+$lang['stuffs_mv_nb_images'] = 'Maximum aantal weer te geven foto\'s';
+$lang['stuffs_mv_category_display'] = 'Toon op categorieÃ«n-pagina\'s';
+$lang['stuffs_mv_all_cats'] = 'Alle categorieÃ«n';
+$lang['stuffs_mv_w_thumb'] = 'Alleen categorieÃ«n met thumbnails';
+$lang['stuffs_mv_wo_thumb'] = 'Alleen categorieÃ«n zonder thumbnails';
+$lang['stuffs_mv_selected_cats'] = 'Alleen geselecteerde categorieÃ«n';
 
-$lang['stuffs_mv_cat_selection'] = 'Categorieën selectie';
+$lang['stuffs_mv_cat_selection'] = 'Selectie categorieÃ«n';
 $lang['stuffs_mv_hold_ctrl'] = 'CTRL ingedrukt houden';
 
 //------------------------------------------------------------------------------
@@ -121,28 +121,28 @@ $lang['stuffs_mv_hold_ctrl'] = 'CTRL ingedrukt houden';
 //------------------------------------------------------------------------------
 
 $lang['module_name_bestrated'] = 'Best gewaardeerd';
-$lang['module_desc_bestrated'] = 'Toon de x best gewaardeerde afbeeldingen uit het album of de categorie';
+$lang['module_desc_bestrated'] = 'Toon de x best gewaardeerde foto\'s uit het album of de categorie';
 
 //------------------------------------------------------------------------------
 //              Module Recent
 //------------------------------------------------------------------------------
 
-$lang['module_name_recent'] = 'Recente afbeeldingen';
-$lang['module_desc_recent'] = 'Toon de x recentste afbeeldingen uit het album of de categorie';
+$lang['module_name_recent'] = 'Recente foto\'s';
+$lang['module_desc_recent'] = 'Toon de x recentste foto\'s uit het album of de categorie';
 
 //------------------------------------------------------------------------------
 //              Module Random
 //------------------------------------------------------------------------------
 
-$lang['module_name_random'] = 'Willekeurige afbeeldingen';
-$lang['module_desc_random'] = 'Toon x willekeurige afbeeldingen uit het album of de categorie';
+$lang['module_name_random'] = 'Willekeurige foto\'s';
+$lang['module_desc_random'] = 'Toon x willekeurige foto\'s uit het album of een categorie';
 
 //------------------------------------------------------------------------------
 //              Module Citation
 //------------------------------------------------------------------------------
 
-$lang['module_name_citation'] = 'Willekeurige quote';
-$lang['module_desc_citation'] = 'Toon een willekeurige quote';
+$lang['module_name_citation'] = 'Willekeurige citaten';
+$lang['module_desc_citation'] = 'Toon een willekeurig aantal';
 $lang['module_Citation_lang'] = 'Taal';
 $lang['module_Citation_code'] = 'Gerelateerde code';
 
@@ -151,5 +151,5 @@ $lang['module_Citation_code'] = 'Gerelateerde code';
 //------------------------------------------------------------------------------
 
 $lang['module_name_featuredphotos'] = 'Uitgelichte afbeeldingen';
-$lang['module_desc_featuredphotos'] = 'Toon de favoriete afbeeldingen van de beheerder';
-$lang['stuffs_mv_recent_period_only'] = 'Laat alleen recente afbeeldingen zien';
+$lang['module_desc_featuredphotos'] = 'Toon de favoriete foto\'s van de webmaster';
+$lang['stuffs_mv_recent_period_only'] = 'Laat alleen recente foto\'s zien';
