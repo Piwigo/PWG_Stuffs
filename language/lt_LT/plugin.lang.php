18203efcd5f1a68c23ab299db8de601ebcbba093
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-02 06:29:02 +0200

    Add lt_LT, thanks to dalius

---
+++ language/lt_LT/plugin.lang.php
@@ -0,0 +1,61 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['stuffs_mv_all_cats'] = 'Visos kategorijos';
+$lang['stuffs_delete'] = 'Trinti bloką';
+$lang['stuffs_desc'] = 'Aprašymas';
+$lang['stuffs_edit'] = 'Keisti bloką';
+$lang['stuffs_edit_mod'] = 'Redaguoti bloką';
+$lang['stuffs_error_no_mod_selected'] = 'Jūs nepasirinkote jokio bloko';
+$lang['stuffs_main_block'] = 'Pagrindinis blokas';
+$lang['stuffs_module_config'] = 'Bloko konfigūracija';
+$lang['module_desc_tags'] = 'Rodyti galerijos žymas';
+$lang['module_name_bestrated'] = 'Geriasia įvertinta';
+$lang['module_name_citation'] = 'Atsitiktinė citata';
+$lang['module_name_featuredphotos'] = 'Teminės nuotraukos';
+$lang['module_name_lastcoms'] = 'Paskutiniai komentarai';
+$lang['module_name_mostvisited'] = 'Dažniausia lankyta';
+$lang['stuffs_add_module'] = 'Pridėti naują bloką';
+$lang['module_name_logon'] = 'Prisijungti';
+$lang['module_name_personal'] = 'Asmeninis blokas';
+$lang['module_name_random'] = 'Atsitiktinės nuotraukos';
+$lang['module_name_recent'] = 'Naujausios nuotraukos';
+$lang['module_name_tags'] = 'Žymės';
+$lang['personal_content'] = 'turinys';
+$lang['stuffs_action'] = 'Veiksmai';
+$lang['stuffs_add_mod'] = 'Pridėti bloką';
+$lang['module_desc_logon'] = 'Rodyti prisijungimo bloką';
+$lang['module_desc_lastcoms'] = 'Rodyti paskutinius paskelbtus komentarus';
+$lang['module_desc_featuredphotos'] = 'Rodyti mėgstamiausias "webmaster"-io nuotraukas';
+$lang['module_desc_citation'] = 'Rodyti atsitiktinę citatą';
+$lang['module_Citation_code'] = 'Susijęs kodas';
+$lang['logon_remove_menubar_block'] = 'Pašalinti "Quick connect" bloką iš meniu juostos';
+$lang['lastcoms_taille_cadres'] = 'Rėmelio aukštis';
+$lang['lastcoms_show_buttons'] = 'Rodyti "admin" mygtukus';
+$lang['lastcoms_no_nb_comments'] = 'Jūs turite nurodyti komentarų skaičių';
+$lang['lastcoms_nb_comments'] = 'Rodomų komentarų skaičius';
+$lang['lastcoms_dim'] = '"Thumbnails" dydis';
+$lang['Hide on home page'] = 'Slėpti pagrindiniame puslapyje';
+$lang['module_Citation_lang'] = 'Kalba';
+$lang['lastcoms_width'] = 'Maksimalus plotis';
+$lang['lastcoms_config'] = 'Konfigūravimas';
+$lang['lastcoms_height'] = 'Maksimalus aukštis';
