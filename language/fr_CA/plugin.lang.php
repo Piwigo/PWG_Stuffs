f7a073b2eb21aafea78f7041e6c29421be760364
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-10 06:22:18 +0100

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['stuffs_tags_default_display'] = 'Affichage des mots-clés';
+$lang['stuffs_mv_w_thumb'] = 'Albums avec miniatures uniquement';
+$lang['stuffs_mv_wo_thumb'] = 'Albums sans miniatures uniquement';
+$lang['stuffs_show_on_cats'] = 'Sur toutes les pages des albums';
+$lang['stuffs_show_on_root'] = 'A la racine des albums';
+$lang['stuffs_tags_cloud'] = 'Nuage de mots-clés';
+$lang['stuffs_edit_mod'] = 'Édition du bloc';
+$lang['stuffs_mv_all_cats'] = 'Tous les albums';
+$lang['stuffs_mv_cat_selection'] = 'Sélection des albums';
+$lang['stuffs_mv_category_display'] = 'Affichage sur les pages des albums';
+$lang['stuffs_mv_selected_cats'] = 'Albums sélectionnés uniquement';
+$lang['module_desc_random'] = 'Affiche x images au hasard dans la galerie ou de l\'album';
+$lang['module_desc_recent'] = 'Affiche les images récentes de la galerie ou de l\'album (s\'il y en a)';
+$lang['module_desc_tags'] = 'Affiche les mots-clés de la galerie';
+$lang['module_name_tags'] = 'Mots-clés';
+$lang['stuffs_confirm_delete'] = 'Êtes-vous sur de vouloir supprimer le bloc %s?';
+$lang['lastcoms_no_nb_comments'] = 'Vous devez remplir le champ "Nombre de commentaires à afficher".';
+$lang['module_desc_bestrated'] = 'Affiche les x images les mieux notées de la galerie ou de l\'album';
+$lang['module_desc_featuredphotos'] = 'Affiche les images favorites du webmestre';
+$lang['module_desc_lastcoms'] = 'Affiche les derniers commentaires envoyés';
+$lang['module_desc_mostvisited'] = 'Affiche les x images les plus vues de la galerie ou de l\'album';
